dd342d8ad5cb03fdaf57b83f2a318afe7b857067
NathanNovak <nnovak@ku.edu>
2019-02-27 08:59:24 -0700

    Added UserProfile.js

---
+++ src/services/user/UserProfile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const baseUrl= 'http://strabospot.org/db';
+
+// export const getUserProfile = async (username) => {
+//   const userProfileBaseUrl = baseUrl.slice(0, baseUrl.lastIndexOf('/'));
+//   try {
+//     let response = await fetch(userProfileBaseUrl + 'profile');
+//     console.log(response);
+//     // let responseJson = await response.json();
+//     return await response.json();
+//   }
+//   catch {
+//
+//   }
+// };
