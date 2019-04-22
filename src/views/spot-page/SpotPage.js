d9d155c9320dd8f0062f4455008f65469bb3a771
NathanNovak <nnovak@ku.edu>
2019-04-22 08:21:13 -0700

    NotebookHeader: added styling to the header and spotName in page.  Added navigation to Photos and Home. Connected Redux

--- src/views/spot-page/SpotPage.js
+++ src/views/spot-page/SpotPage.js
@@ -1,20 +1,42 @@
 import React from 'react'
+import classes from './SpotPageStyles';
 import {
   ActivityIndicator,
+  Button,
+  Dimensions,
+  Text,
   View,
-  StyleSheet, Alert,
+  StyleSheet
 } from 'react-native'
-import {Header} from 'react-native-elements';
-import Aux from '../../shared/AuxWrapper';
+import * as actions from "../../store/actions";
+import {
+  CURRENT_BASEMAP,
+  FEATURES_UPDATED,
+  FEATURE_ADD,
+  FEATURE_DELETE,
+  FEATURE_SELECTED,
+  EDIT_SPOT
+} from '../../store/Constants';
+import {Header, Divider, Input} from 'react-native-elements';
 import {connect} from 'react-redux';
 import {Navigation} from 'react-native-navigation/lib/dist/index';
 import ButtonNoBackground from '../../ui/ButtonNoBackround';
 import Loading from '../../ui/Loading';
 
+const width = Dimensions.get('window').width
+
 class SpotPage extends React.Component {
   _isMounted = false;
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: null
+    }
+  }
+
   async componentDidMount() {
+    console.log('SpotPage mounted', actions);
     this._isMounted = true;
     // console.log('SpotPage Mounted', this.props.featureCollectionSelected)
     // const spotData = await this.props.featureCollectionSelected.features.map(spot => {
@@ -32,58 +54,84 @@ class SpotPage extends React.Component {
   }
 
   render() {
-    // let header = (
-    //   <Aux>
-    //     <Header
-    //       backgroundColor={'lightgrey'}
-    //       centerComponent={{text: 'No Spot Found', style: {color: 'black', fontSize: 25}}}
-    //     >
-    //       <ButtonNoBackground onPress={() => Navigation.pop(this.props.componentId)}>Home</ButtonNoBackground>
-    //     </Header>
-    //     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-    //       <Loading animating={animating}/>
-    //     </View>
-    //   </Aux>);
 
-    // if (this.state.selectedSpot) {
-    //   header = (
-    //     <Header
-    //       backgroundColor={'lightgrey'}
-    //       centerComponent={{
-    //         text: this.props.selectedSpot.properties.name,
-    //         style: {color: 'black', fontSize: 25}
-    //       }}
-    //     >
-    //       <ButtonNoBackground onPress={() => Navigation.pop(this.props.componentId)}>Home</ButtonNoBackground>
-    //     </Header>
-    //   )
-    // }
+    const images = {
+      component: {
+        name: 'Images',
+        passProps: {
+          text: 'Pushed screen'
+        },
+      }
+    };
+
+    const home = {
+      component: {
+        name: 'Home',
+        passProps: {
+          text: 'Pushed screen'
+        },
+      }
+    }
+
     return (
-      <View style={styles.container}>
-        <Header
-          backgroundColor={'lightgrey'}
-          centerComponent={{
-            text: this.props.selectedSpot.properties.name,
-            style: {color: 'black', fontSize: 25}
-          }}
-        >
-          <ButtonNoBackground onPress={() => Navigation.pop(this.props.componentId)}>Home</ButtonNoBackground>
-        </Header>
-        {/*<View>*/}
-        {/*  <TextInput value={this.state.selectedSpot} onChange={(text) => this.setState({selectedSpot: {name: text}})}/>*/}
-        {/*</View>*/}
+      <View>
+        <View>
+          <Header
+            style={classes.container}
+            backgroundColor={'lightgrey'}>
+            <ButtonNoBackground
+              iconName={'ios-arrow-back'}
+              iconType={'ionicon'}
+              iconStyle={{color: 'black'}}
+              style={{flexDirection: 'row'}}
+              textStyle={{marginLeft: 10, paddingTop: 1, fontSize: 18}}
+              onPress={() => Navigation.pop(this.props.componentId)}>
+              Back
+            </ButtonNoBackground>
+            <Text
+              style={{marginBottom: 15, fontSize: 24}}
+            >
+              {/*{this.state.text}*/}
+              {this.props.selectedSpot.properties.name}
+            </Text>
+            <View style={{flexDirection: 'row'}}>
+              <ButtonNoBackground
+                onPress={() => Navigation.push(this.props.componentId, images)}
+                style={{justifyContent: 'center',}}
+                iconName={'ios-camera'}
+                iconType={'ionicon'}
+                size={40}
+                iconStyle={{color: 'black', marginTop: 10}}/>
+              <ButtonNoBackground
+                onPress={() => Navigation.push(this.props.componentId, home)}
+                style={{justifyContent: 'center', marginRight: 20}}
+                iconName={'map-o'}
+                iconType={'font-awesome'}
+                size={25}
+                iconStyle={{color: 'black', marginTop: 10}}/>
+            </View>
+          </Header>
+        </View>
+        <View style={classes.spotContainer}>
+          <Text>Spot Name:</Text>
+          <Input
+            placeholder={this.props.selectedSpot.properties.name}
+            // style={{height: 40, borderColor: 'gray', borderWidth: 10}}
+            onChangeText={(text) => this.props.onSpotEdit('name', text)}
+            // onChangeText={(text) => this.setState({text})}
+            // value={this.props.selectedSpot.properties.name}
+            />
+          <Button
+            title={'Save'}
+            onPress={() => this.setState(this.state.text)}
+          />
+        </View>
+        {/*<Divider style={{width: width, height: 15, backgroundColor: 'lightgrey'}}/>*/}
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
 
 function mapStateToProps(state) {
   return {
@@ -92,4 +140,9 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(SpotPage);
+const mapDispatchToProps = {
+    onFeatureAdd: (feature) => ({type: FEATURE_ADD, feature: feature}),
+    onSpotEdit: (field, value) => ({type: EDIT_SPOT, field: field, value: value})
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SpotPage);
