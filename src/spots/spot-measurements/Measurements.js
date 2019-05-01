54846ed1315b5abd3c097be37d150575a7e861b3
NathanNovak <nnovak@ku.edu>
2019-05-01 14:59:55 -0700

    Measurements: added back button and connected redux

--- src/spots/spot-measurements/Measurements.js
+++ src/spots/spot-measurements/Measurements.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {connect} from 'react-redux';
 import Compass from '../../components/compass/Compass';
+import styles from './MeasurementsStyles';
+import {SpotPages} from "../../components/notebook-panel/Notebook.constants";
+import {Button} from "react-native-elements";
+import {EDIT_SPOT_PROPERTIES, FEATURE_ADD, SET_SPOT_PAGE_VISIBLE} from "../../store/Constants";
 
 const MeasurementPage = (props) => {
   return (
     <React.Fragment>
-      <Text>This is the measurements page</Text>
-      <View >
+      <Button
+        icon={{
+          name: 'arrow-back',
+          size: 20,
+          color: 'black'
+        }}
+        containerStyle={{marginTop: 10}}
+        titleStyle={{color: 'blue'}}
+        title={'Return to Overview'}
+        type={'clear'}
+        onPress={() => props.setPageVisible(SpotPages.OVERVIEW)}
+      />
+      {/*<Text>This is the measurements page</Text>*/}
+      <View style={styles.compassContainer}>
         <Compass/>
       </View>
     </React.Fragment>
   );
 };
 
-export default MeasurementPage;
+const mapDispatchToProps = {
+  setPageVisible: (page) => ({type: SET_SPOT_PAGE_VISIBLE, page: page })
+};
+
+export default connect(null, mapDispatchToProps)(MeasurementPage);
---
+++ src/spots/spot-measurements/MeasurementsStyles.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  compassContainer: {
+    flex: 1,
+    position: 'absolute',
+    top: 50
+    // width: 200,
+    // height: 200
+  }
+});
+
+export default styles;
