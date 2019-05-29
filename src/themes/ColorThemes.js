608622bb6a5e1874eb964e1d27bfcdaaae52fce2
NathanNovak <nnovak@ku.edu>
2019-05-29 06:22:48 -0700

    Theme: changed Lightgrey color theme to RGB color per design

--- src/themes/ColorThemes.js
+++ src/themes/ColorThemes.js
@@ -1,6 +1,6 @@
 //See https://facebook.github.io/react-native/docs/colors
 
-export const LIGHTGREY = 'lightgray';  //(#d3d3d3)
+export const LIGHTGREY = '#efeff4';  //(lightgrey)
 export const MEDIUMGREY = 'darkgray';  //(#d3d3d3)
 export const DARKGREY = 'dimgray';
 //export const BLUE = 'blue';  // (#0000ff)
