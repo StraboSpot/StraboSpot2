58bcaddd37135ec17a83d3302b5a2dd13be8f925
NathanNovak <nnovak@ku.edu>
2019-06-17 14:11:32 -0700

    Samples: created view for modal and connected redux

--- src/components/samples/Samples.view.js
+++ src/components/samples/Samples.view.js
@@ -1,35 +1,101 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-import NotebookBackButton from '../notebook-panel/ui/ReturnToOverviewButton';
-import {SpotPages} from "../notebook-panel/Notebook.constants";
-import * as actionCreators from "../../store/actions";
+import React, {useState} from 'react';
+import {Text, TextInput, View} from 'react-native';
 import {connect} from "react-redux";
+import {ButtonGroup, Input} from "react-native-elements";
+import styles from './samples.style';
+import Slider from '../../shared/ui/Slider';
+import {EDIT_SPOT_PROPERTIES} from "../../store/Constants";
 
-const samplesView = (props) => {
+const samplesModalView = (props) => {
+  let count = 0;
+  // const [count, setCount] = useState(0);
+  // const [sliderValue, setSliderValue] = useState(3);
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const [sampleOrientedValue, setSampleOrientedValue] = useState(0);
+  const [inplaceness, setInplaceness] = useState(null);
+  const [label, setLabel] = useState('sample');
+  const [name, setName] = useState(null);
+  const [note, setNote] = useState(null);
+  const [description, setDescription] = useState(label);
+
+  const buttonNames = ['Oriented', 'Unoriented'];
+
+  const buttonSelected = (selectedButton) => {
+   setSelectedButton(selectedButton);
+    let sampleOriented = selectedButton === 0 ? 'yes' : 'no';
+    setInplaceness(sampleOriented)
+  };
+
+  const saveSample = () => {
+    sample = [];
+    sample.push({
+      inplaceness_of_sample: inplaceness,
+      lable: label,
+      sample_id_name: name,
+      sample_notes: note,
+      sample_description: name
+    })
+  };
+
   return (
     <React.Fragment>
-      <NotebookBackButton
-        onPress={() => {
-          const pageVisible = props.setPageVisible(SpotPages.OVERVIEW);
-          if (pageVisible.page !== SpotPages.SAMPLE) {
-            props.showModal('isSamplesModalVisible', false);
-          }
-        }}
-      />
-      <Text style={{justifyContent: 'center', alignContent: 'center'}}>SAMPLES PAGE</Text>
+      <View style={styles.input}>
+        <Input
+          placeholder={'Name'}
+          inputContainerStyle={styles.inputContainer}
+          inputStyle={styles.inputText}
+          onChangeText={(text) => setName({text})}
+        />
+        <Input
+          placeholder={'Label'}
+          inputContainerStyle={styles.inputContainer}
+          inputStyle={styles.inputText}
+        />
+        <View style={styles.textboxContainer}>
+          <TextInput
+            placeholder={'Description'}
+            maxLength={120}
+            multiline={true}
+            numberOfLines={4}
+            style={styles.textbox}
+          />
+        </View>
+      </View>
+      <View style={{flex: 15, paddingBottom: 10, alignItems: 'center'}}>
+        <View style={{alignItems: 'center'}}>
+          <Text style={styles.text}>Inplaceness of Sample</Text>
+        </View>
+        <Slider
+          sliderValue={sampleOrientedValue}
+          setSliderValue={value => setSampleOrientedValue(value)}
+          leftText={'Float'}
+          rightText={'In Place'}
+        />
+      </View>
+      <View style={{flex: 10}}>
+        <ButtonGroup
+          onPress={(value) => buttonSelected(value)}
+          selectedIndex={selectedButton}
+          buttons={buttonNames}
+        />
+      </View>
+      <View style={[styles.textboxContainer ,{flex: 20}]}>
+        <TextInput
+          placeholder={'Description'}
+          maxLength={120}
+          multiline={true}
+          numberOfLines={4}
+          style={styles.textbox}
+        />
+        <Text>{name}</Text>
+      </View>
     </React.Fragment>
   );
 };
 
-function mapStateToProps(state) {
-  return {
-    spot: state.home.selectedSpot
-  }
-}
-
 const mapDispatchToProps = {
-  setPageVisible: (page) => (actionCreators.setSpotPageVisible(page))
+  onSpotEdit: (field, value) => ({type: EDIT_SPOT_PROPERTIES, field: field, value: value}),
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(samplesView);
-
+export default connect()(samplesModalView);
--- src/components/samples/SamplesModal.view.js
+++ src/components/samples/SamplesModal.view.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 import Modal from '../../shared/ui/modal/Modal.view';
-import styles from './samples.styles';
+import styles from './samples.style';
+import Samples from './Samples.view';
 
 const samplesModalView = (props) => {
   console.log('SampleModal props', props)
   return (
     <Modal
+      component={<Samples/>}
       close={() => props.close('isSamplesModalVisible')}
-      textStyle={{fontWeight: 'bold' }}
+      buttonTitleRight={'Cancel'}
+      onPress={props.cancel}
       style={styles.samplesContainer}
     >
-      Samples Modal Content
+      New Sample
     </Modal>
   );
 };
