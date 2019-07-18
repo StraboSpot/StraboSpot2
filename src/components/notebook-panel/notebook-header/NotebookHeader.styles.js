fa224eacbdf17106eece21ac322d8ca375b023da
NathanNovak <nnovak@ku.edu>
2019-07-18 11:41:33 -0700

    additional to previous push NotebookHeaderStyles: made header smaller

--- src/components/notebook-panel/notebook-header/NotebookHeader.styles.js
+++ src/components/notebook-panel/notebook-header/NotebookHeader.styles.js
@@ -2,40 +2,34 @@ import {StyleSheet} from 'react-native';
 import * as themes from '../../../shared/styles.constants';
 
 const notebookHeaderStyles = StyleSheet.create({
-    headerContentContainer: {
-      flex: 15,
-      flexDirection: 'row',
-    },
-    headerButtons: {
-      paddingTop: 20
-    },
-    headerCoords: {
-      color: themes.PRIMARY_ACCENT_COLOR,
-      fontSize: 16
-    },
-    headerImage: {
-      resizeMode: 'contain',
-    },
-    headerCoordsContainer: {
-      height: 30,
-      marginLeft: 5
-    },
-    headerSpotName: {
-      fontSize: 35,
-      fontWeight: 'bold'
-    },
-    headerSpotNameContainer: {
-      height: 50
-    },
-    headerSymbolIcon: {
-      padding: 5
-    },
-    headerSpotNameAndCoordsContainer: {
-      paddingLeft: 10,
-      paddingRight: 10,
-      flex: 8,
-      flexDirection: 'column'
-    }
+  headerContentContainer: {
+    flex: 15,
+    flexDirection: 'row',
+  },
+  headerButtons: {
+    paddingTop: 20
+  },
+  headerCoords: {
+    color: themes.PRIMARY_ACCENT_COLOR,
+    fontSize: 16
+  },
+  headerImage: {
+    resizeMode: 'contain',
+  },
+  headerSpotName: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  headerSpotNameAndCoordsContainer: {
+    paddingLeft: 10,
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'column',
+  },
+  threeDotMenu: {
+    width: 25,
+    height: 25,
+  }
 });
 
 export default notebookHeaderStyles;
