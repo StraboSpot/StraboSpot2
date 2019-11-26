727218fd1200e77b68a382a16e917b4336787107
NathanNovak <nnovak@ku.edu>
2019-11-26 08:39:59 -0700

    OverView: removed ScrollView and replaced with Flatlist

--- src/components/notebook-panel/Overview.js
+++ src/components/notebook-panel/Overview.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {Text, ScrollView, TouchableOpacity, View} from 'react-native';
+import React, {useState, Component} from 'react';
+import {Text, ScrollView, FlatList, TouchableOpacity, View} from 'react-native';
 
 import TagsOverview from '../tags/TagsOverview';
 import NotesOverview from '../notes/NotesOverview';
@@ -15,18 +15,22 @@ const Overview = props => {
 
   const SECTIONS = [
     {
+      id: 1,
       title: 'Measurements',
       content: <MeasurementsOverview/>,
     },
     {
+      id: 2,
       title: 'Photos and Sketches',
       content: <NotebookImages/>,
     },
     {
+      id: 3,
       title: 'Tags',
       content: <TagsOverview/>,
     },
     {
+      id: 4,
       title: 'Notes',
       content: <NotesOverview/>,
     },
@@ -51,27 +55,39 @@ const Overview = props => {
     else setCollapsedSections(collapsedSections.concat(name));
   };
 
-  return (
-    <ScrollView>
-      {SECTIONS.map((section, index) => {
-        return (
-          <View key={section.title}>
-            <TouchableOpacity onPress={() => toggleCollapsed(section.title)}>
-              <View style={notebookStyles.collapsibleSectionHeaderContainer}>
-                {collapsedSections.includes(section.title) ? expandedIcon : collapseIcon}
-                <Text style={notebookStyles.collapsibleSectionHeaderText}>{section.title}</Text>
-              </View>
-            </TouchableOpacity>
-            {/*<Collapsible collapsed={false} align="center">*/}
-            <Collapsible collapsed={collapsedSections.includes(section.title)} align='center'>
-              <View>
-                {section.content}
-              </View>
-            </Collapsible>
+  const renderCollapsableList = () => {
+
+    return(
+      <FlatList
+        keyExtractor={(section) => section.id.toString()}
+        data={SECTIONS}
+        renderItem={({item}) => renderSections(item)}/>
+    );
+  };
+
+  const renderSections = (section) => {
+    return (
+      <View key={section.title}>
+        <TouchableOpacity onPress={() => toggleCollapsed(section.title)}>
+          <View style={notebookStyles.collapsibleSectionHeaderContainer}>
+            {collapsedSections.includes(section.title) ? expandedIcon : collapseIcon}
+            <Text style={notebookStyles.collapsibleSectionHeaderText}>{section.title}</Text>
           </View>
-        );
-      })}
-    </ScrollView>
+        </TouchableOpacity>
+        {/*<Collapsible collapsed={false} align="center">*/}
+        <Collapsible collapsed={collapsedSections.includes(section.title)} align='center'>
+          <View>
+            {section.content}
+          </View>
+        </Collapsible>
+      </View>
+    );
+  };
+
+  return (
+    <View>
+      {renderCollapsableList()}
+    </View>
   );
 };
 
