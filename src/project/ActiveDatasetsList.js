0e6370cc9a87f15da9c59e6b839e1a0ea7e09196
NathanNovak <nnovak@ku.edu>
2019-12-10 19:07:29 -0700

    ActiveDatasetsList.js: changed projectDatasets -> datasets made an object where the dataset ids are the key. Changed constant names

--- src/project/ActiveDatasetsList.js
+++ src/project/ActiveDatasetsList.js
@@ -6,18 +6,17 @@ import {projectReducers} from './Project.constants';
 
 const ActiveDatasetsList = () => {
   const [refresh] = useState();
-  const projectDatasets = useSelector(state => state.project.projectDatasets);
+  const datasets = useSelector(state => state.project.datasets);
   const dispatch = useDispatch();
 
-  const makeDatasetCurrent = (index) => {
-    projectDatasets.map(data => data.current = false);
-    const selectedDataset = JSON.parse(JSON.stringify(projectDatasets));
-    selectedDataset[index].current = !selectedDataset[index].current;
-    console.table(selectedDataset);
-    dispatch({type: projectReducers.DATASETS.PROJECT_DATASETS, datasets: selectedDataset});
+  const makeDatasetCurrent = (id) => {
+    Object.values(datasets).map(data => data.current = false);
+    const datasetsCopy = JSON.parse(JSON.stringify(datasets));
+    datasetsCopy[id].current = !datasetsCopy[id].current;
+    dispatch({type: projectReducers.DATASETS.DATASETS_UPDATE, datasets: datasetsCopy});
   };
 
-  const renderActiveDatasets = ({item, index}) => {
+  const renderActiveDatasets = ({item}) => {
     if (item.active) {
       return (
         <ListItem
@@ -25,8 +24,8 @@ const ActiveDatasetsList = () => {
           title={item.name}
           containerStyle={{padding: 10}}
           bottomDivider
-          checkmark={projectDatasets[index].current}
-          onPress={() => makeDatasetCurrent(index)}
+          checkmark={item.current}
+          onPress={() => makeDatasetCurrent(item.id)}
         />
       );
     }
@@ -35,9 +34,9 @@ const ActiveDatasetsList = () => {
   return (
     <React.Fragment>
       <FlatList
-        keyExtractor={(item, i) => item.id.toString()}
+        keyExtractor={(item) => item.id.toString()}
         extraData={refresh}
-        data={projectDatasets}
+        data={Object.values(datasets)}
         renderItem={renderActiveDatasets}
       />
     </React.Fragment>
