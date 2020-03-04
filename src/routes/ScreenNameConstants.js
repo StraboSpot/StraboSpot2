388d5a044ec41f470f8216a437873d81429b1d57
NathanNovak <nnovak@ku.edu>
2020-03-04 13:51:13 -0700

    routes: removed folder with contents because of non-use
    Signed-off-by: NathanNovak <nnovak@ku.edu>

--- src/routes/ScreenNameConstants.js
+++
@@ -1,7 +0,0 @@
-export const HOME = 'Home';
-export const INITALIZING = 'Initializing';
-export const SIGN_IN = 'SignIn';
-export const SIGN_UP = 'SignUp';
-export const SPOT_PAGE = 'SpotPage';
-export const IMAGEGALLERY = 'ImageGallery';
-export const IMAGEINFO = 'ImageInfo';
