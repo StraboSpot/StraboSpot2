d37803771c8dc128f11cedc39430570478089f29
NathanNovak <nnovak@ku.edu>
2020-03-16 06:33:51 -0700

    ProjectDescriptionEditModal.js: Added file to project
    Signed-off-by: NathanNovak <nnovak@ku.edu>

---
+++ src/modules/project/ProjectDescriptionEditModal.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Dialog, {
+  DialogTitle,
+  DialogButton,
+  DialogContent,
+  DialogFooter,
+  SlideAnimation,
+  FadeAnimation,
+} from 'react-native-popup-dialog';
+import styles from '../../shared/common.styles';
+
+const DescriptionEditingModal = (props) => {
+
+  return (
+    <React.Fragment>
+      <Dialog
+        dialogStyle={styles.dialogBox}
+        width={300}
+        visible={props.visible}
+        dialogAnimation={new FadeAnimation({
+          animationDuration: 300,
+          useNativeDriver: true,
+        })}
+        onTouchOutside={props.onTouchOutside}
+        useNativeDriver={true}
+        dialogTitle={
+          <DialogTitle
+            style={props.style}
+            textStyle={styles.dialogTitleText}
+            title={props.dialogTitle}/>
+        }
+        footer = {
+          <DialogFooter>
+            <DialogButton onPress={props.confirm} text={'Ok'}/>
+            <DialogButton onPress={props.cancel} text={'Cancel'}/>
+          </DialogFooter>
+        }
+      >
+        <DialogContent style={[styles.dialogContent, props.dialogContent]}>
+          {props.children}
+        </DialogContent>
+      </Dialog>
+    </React.Fragment>
+  );
+};
+
+export default DescriptionEditingModal;
