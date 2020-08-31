db66519c9026fe8781734f7e349f4c77613fbe1e
NathanNovak <nnovak@ku.edu>
2020-08-31 05:45:01 -0700

    Added index.js to /images
    Signed-off-by: NathanNovak <nnovak@ku.edu>

---
+++ src/modules/images/index.js
@@ -0,0 +1,7 @@
+export {default as ImageGallery} from './ImageGallery';
+export {default as ImageInfo} from './ImageInfo';
+export {default as ImageOverview} from './ImageNotebook';
+export {default as ImageProperties} from './ImagePropertiesModal';
+export {default as imageStyles} from './images.styles';
+export {default as ImagesPage} from './ImagesViewPage';
+export {default as useImagesHook} from './useImages';
