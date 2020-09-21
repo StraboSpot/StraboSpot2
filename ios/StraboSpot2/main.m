fa847e6a917a7c7479f8cd815207a5087e1c09c8
NathanNovak <nnovak@ku.edu>
2020-09-21 12:52:51 -0700

    Added line to to resolve cocoapods issue and removed return in main.m
    Signed-off-by: NathanNovak <nnovak@ku.edu>

--- ios/StraboSpot2/main.m
+++ ios/StraboSpot2/main.m
@@ -3,7 +3,6 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-  @autoreleasepool {
-    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  @autoreleasepool {    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
 }
