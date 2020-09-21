19db0f5609c03c49ff910907c63c18ce47e8b747
NathanNovak <nnovak@ku.edu>
2020-09-21 13:38:24 -0700

    Fixed 'key' warning
    Signed-off-by: NathanNovak <nnovak@ku.edu>

--- src/modules/main-menu-panel/MainMenuPanelList.js
+++ src/modules/main-menu-panel/MainMenuPanelList.js
@@ -9,19 +9,21 @@ import styles from './mainMenuPanel.styles';
 
 const MainMenuPanelList = props => {
 
-  const renderButtons = (name, key) => {
+  const renderButtons = (name, i) => {
     return (
-      <Button
-        key={key}
-        title={name === MAIN_MENU_ITEMS.MANAGE.ACTIVE_PROJECTS
-          ? MAIN_MENU_ITEMS.MANAGE.ACTIVE_PROJECTS + ` (${props.activeProject})`
-          : name
-        }
-        type={'clear'}
-        containerStyle={styles.navItemStyle}
-        titleStyle={styles.navButtonText}
-        onPress={() => props.onPress(name)}
-      />
+      <View key={i}>
+        <Button
+
+          title={name === MAIN_MENU_ITEMS.MANAGE.ACTIVE_PROJECTS
+            ? MAIN_MENU_ITEMS.MANAGE.ACTIVE_PROJECTS + ` (${props.activeProject})`
+            : name
+          }
+          type={'clear'}
+          containerStyle={styles.navItemStyle}
+          titleStyle={styles.navButtonText}
+          onPress={() => props.onPress(name)}
+        />
+      </View>
     );
   };
 
@@ -29,9 +31,9 @@ const MainMenuPanelList = props => {
     <ScrollView>
       <View style={styles.navSectionStyle}>
         {Object.entries(MAIN_MENU_ITEMS).map(([item, submenuItems]) => (
-          <View>
+          <View key={item}>
             <SectionDivider dividerText={item}/>
-            {Object.values(submenuItems).map(submenuItem => renderButtons(submenuItem))}
+            {Object.values(submenuItems).map((submenuItem, index) => renderButtons(submenuItem, index))}
           </View>
         ))}
       </View>
