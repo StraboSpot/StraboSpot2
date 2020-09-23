4e4de1a03ccced7ee4fea04f7897c415b94fa081
NathanNovak <nnovak@ku.edu>
2020-09-23 08:44:04 -0700

    Added Route.js to App.js to access store for isSignedIn reducer.
    Signed-off-by: NathanNovak <nnovak@ku.edu>

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 
-import {NavigationContainer} from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import * as Sentry from '@sentry/react-native';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 
-import Home from './src/modules/home/Home';
-import ImageInfo from './src/modules/images/ImageInfo';
-import SignIn from './src/modules/sign-in/SignIn';
-import SignUp from './src/modules/sign-up/SignUp';
-import Sketch from './src/modules/sketch/Sketch';
+import Route from './src/routes/Routes';
 import Loading from './src/shared/ui/Loading';
 import configureStore from './src/store/ConfigureStore';
 
@@ -19,31 +13,16 @@ Sentry.init({
   dsn: 'https://4a3776035f3d425c997d45dc6d05e659@o201723.ingest.sentry.io/1783328',
 });
 
-const Stack = createStackNavigator();
-
 const App = () => {
-
-  const navigationOptions = {
-    gestureEnabled: false,
-    headerShown: false,
-  };
 
   const {store, persistor} = configureStore();
 
   return (
-      <Provider store={store}>
-        <PersistGate loading={<Loading/>} persistor={persistor}>
-          <NavigationContainer>
-          <Stack.Navigator initialRouteName={'SignIn'}>
-            <Stack.Screen name={'SignUp'} component={SignUp} options={navigationOptions}/>
-            <Stack.Screen name={'SignIn'} component={SignIn} options={navigationOptions}/>
-            <Stack.Screen name={'HomeScreen'} component={Home} options={navigationOptions}/>
-            <Stack.Screen name={'ImageInfo'} component={ImageInfo} options={navigationOptions}/>
-            <Stack.Screen name={'Sketch'} component={Sketch} options={navigationOptions}/>
-          </Stack.Navigator>
-          </NavigationContainer>
-        </PersistGate>
-      </Provider>
+    <Provider store={store}>
+      <PersistGate loading={<Loading/>} persistor={persistor}>
+        <Route/>
+      </PersistGate>
+    </Provider>
   );
 };
 
---
+++ src/routes/Routes.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {useSelector} from 'react-redux';
+
+import Home from '../modules/home/Home';
+import ImageInfo from '../modules/images/ImageInfo';
+import SignIn from '../modules/sign-in/SignIn';
+import SignUp from '../modules/sign-up/SignUp';
+import Sketch from '../modules/sketch/Sketch';
+
+const Stack = createStackNavigator();
+const AuthStack = createStackNavigator();
+const AppStack = createStackNavigator();
+
+const navigationOptions = {
+  gestureEnabled: false,
+  headerShown: false,
+};
+
+const AppScreens = () => {
+  const isSignedIn = useSelector(state => state.home.isSignedIn);
+  return (
+    <AppStack.Navigator initialRouteName={isSignedIn ? 'HomeScreen' : 'SignIn'}>
+      <Stack.Screen
+        name={'SignIn'}
+        component={SignIn}
+        options={navigationOptions}
+      />
+      <Stack.Screen
+        name={'SignUp'}
+        component={SignUp}
+        options={navigationOptions}
+      />
+      <Stack.Screen
+        name={'HomeScreen'}
+        component={Home}
+        options={navigationOptions}
+      />
+      <Stack.Screen
+        name={'ImageInfo'}
+        component={ImageInfo}
+        options={navigationOptions}
+      />
+      <Stack.Screen
+        name={'Sketch'}
+        component={Sketch}
+        options={navigationOptions}
+      />
+    </AppStack.Navigator>
+  );
+};
+
+const Route = () => {
+  return (
+    <NavigationContainer>
+      <AppScreens/>
+    </NavigationContainer>
+  );
+};
+
+export default Route;
