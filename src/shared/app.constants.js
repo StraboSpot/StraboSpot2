b07b3e003179395edb6ce5d920921f059ac5fec9
NathanNovak <nnovak@ku.edu>
2020-11-02 06:39:42 -0700

    Versioned to 0.14.1 for small fix in slide show
    Signed-off-by: NathanNovak <nnovak@ku.edu>

--- src/shared/app.constants.js
+++ src/shared/app.constants.js
@@ -18,4 +18,4 @@ export const redux = {
 };
 
 // Be sure to change the version in X-Code to match TODO: Change X-Code version to match
-export const VERSION_NUMBER = 'Version 0.14.0';
+export const VERSION_NUMBER = 'Version 0.14.1';
