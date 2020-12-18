c418dccef7115e4665de215cdf58a4b04a61a43f
NathanNovak <nnovak@ku.edu>
2020-12-18 16:07:57 -0700

    versioned to 0.17.1
    Signed-off-by: NathanNovak <nnovak@ku.edu>

--- src/shared/app.constants.js
+++ src/shared/app.constants.js
@@ -3,4 +3,5 @@ export const REDUX = {
 };
 
 // Be sure to change the version in X-Code to match TODO: Change X-Code version to match
-export const VERSION_NUMBER = 'Version 0.17.0';
+export const VERSION_NUMBER = 'Version 0.17.1';
+
