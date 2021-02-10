98faa3e3e735465d6f008597ea100a3fea54f100
NathanNovak <nnovak@ku.edu>
2021-02-10 13:45:16 -0700

    Data: minor formatting
    Signed-off-by: NathanNovak <nnovak@ku.edu>

--- src/modules/Data/useData.js
+++ src/modules/Data/useData.js
@@ -1,22 +1,23 @@
 import React from 'react';
+
 import {useDispatch, useSelector} from 'react-redux';
 
 import {editedSpotProperties} from '../spots/spots.slice';
 
-const useData = (props) => {
+const useData = () => {
   const dispatch = useDispatch();
   const spot = useSelector(state => state.spot.selectedSpot);
 
   const saveCSV = (CSVObject) => {
-    try{
+    try {
       let savedTables;
       let editedData = spot.properties.data ? JSON.parse(JSON.stringify(spot.properties.data)) : {};
       if (spot.properties.data?.tables) savedTables = spot.properties.data.tables;
-      console.log(savedTables)
+      console.log(savedTables);
       if (!editedData.tables) editedData.tables = [];
       editedData.tables.push(CSVObject);
-      console.log(editedData)
-      dispatch(editedSpotProperties({field: 'data', value: editedData}))
+      console.log(editedData);
+      dispatch(editedSpotProperties({field: 'data', value: editedData}));
     }
     catch (err) {
       console.error('Error saving .CSV file');
