63293e8121017ce9865003925e60a358f5da9074
Sai Krishna Teja D <saikrishnatejad@gmail.com>
2021-02-17 15:06:39 -0600

    filter Tags by tags type

--- src/modules/tags/TagsAtSpotList.js
+++ src/modules/tags/TagsAtSpotList.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import {FlatList} from 'react-native';
+import React, {useState} from 'react';
+import {FlatList, TextInput} from 'react-native';
 
 import {Icon, ListItem} from 'react-native-elements';
 import {useDispatch, useSelector} from 'react-redux';
 
 import commonStyles from '../../shared/common.styles';
+import {isEmpty} from '../../shared/Helpers';
 import {PRIMARY_ACCENT_COLOR} from '../../shared/styles.constants';
 import FlatListItemSeparator from '../../shared/ui/FlatListItemSeparator';
 import ListEmptyText from '../../shared/ui/ListEmptyText';
+import {formStyles} from '../form';
 import {MAIN_MENU_ITEMS, SIDE_PANEL_VIEWS} from '../main-menu-panel/mainMenu.constants';
 import {setMenuSelectionPage, setSidePanelVisible} from '../main-menu-panel/mainMenuPanel.slice';
 import {setSelectedTag} from '../project/projects.slice';
@@ -17,12 +19,20 @@ const TagsAtSpotList = (props) => {
   const [useTags] = useTagsHook();
   const dispatch = useDispatch();
   const isMainMenuPanelVisible = useSelector(state => state.home.isMainMenuPanelVisible);
+  const [searchText,setSearchText] = useState('');
+  const [relevantTags,setRelevantTags] = useState(useTags.getTagsAtSpotGeologicUnitFirst());
 
   const openTag = (tag) => {
     dispatch(setSidePanelVisible({bool: true, view: SIDE_PANEL_VIEWS.TAG_DETAIL}));
     dispatch(setSelectedTag(tag));
     dispatch(setMenuSelectionPage({name: MAIN_MENU_ITEMS.ATTRIBUTES.TAGS}));
     if (!isMainMenuPanelVisible) props.openMainMenu();
+  };
+
+  const searchTagsByType = (tagType) => {
+    const tagsAtSpot = useTags.getTagsAtSpot();
+    setSearchText(tagType);
+    setRelevantTags(useTags.filterTagsByTagType(tagsAtSpot,tagType));
   };
 
   const renderTag = (tag) => {
@@ -47,9 +57,15 @@ const TagsAtSpotList = (props) => {
 
   return (
     <React.Fragment>
+      <TextInput
+        style={formStyles.filterFieldValue}
+        onChangeText={tagType => searchTagsByType(tagType)}
+        value={searchText}
+        placeholder='Search tags by type'
+      />
       <FlatList
         keyExtractor={item => item.id.toString()}
-        data={useTags.getTagsAtSpotGeologicUnitFirst()}
+        data={isEmpty(searchText) ? useTags.getTagsAtSpotGeologicUnitFirst() : relevantTags}
         renderItem={({item}) => renderTag(item)}
         ItemSeparatorComponent={FlatListItemSeparator}
         ListEmptyComponent={<ListEmptyText text='No Tags'/>}
