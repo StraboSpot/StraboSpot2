51ddd577f14958a988bedcb4ab2610e08798e220
NathanNovak <nnovak@ku.edu>
2021-04-05 08:57:34 -0700

    Removed react-native-scale-bar and dependencies. Versioned to 0.32.2
    Signed-off-by: NathanNovak <nnovak@ku.edu>

--- src/modules/maps/Basemaps.js
+++ src/modules/maps/Basemaps.js
@@ -3,7 +3,6 @@ import {Text, View} from 'react-native';
 
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import * as turf from '@turf/turf';
-import ScaleBar from 'react-native-scale-bar';
 import {useSelector} from 'react-redux';
 
 import {isEmpty} from '../../shared/Helpers';
@@ -75,19 +74,12 @@ function Basemap(props) {
 
   return (
     <View style={{flex: 1}}>
-      {currentZoom && <View style={homeStyles.currentZoomContainer}>
-          <Text style={props.basemap.id === 'mapbox.satellite' ? homeStyles.currentZoomTextWhite
-            : homeStyles.currentZoomTextBlack}>
-            Zoom: {currentZoom && currentZoom.toFixed(1)}
-          </Text>
-          <ScaleBar
-            zoom={currentZoom}
-            bottom={0}
-            lineWidth={2}
-            color={props.basemap.id === 'mapbox.satellite' ? 'white' : 'black'}
-          />
-      </View>}
-
+      <View style={homeStyles.currentZoomContainer}>
+        <Text style={props.basemap.id === 'mapbox.satellite' ? homeStyles.currentZoomTextWhite
+          : homeStyles.currentZoomTextBlack}>
+          Zoom: {currentZoom && currentZoom.toFixed(1)}
+        </Text>
+      </View>
       <MapboxGL.MapView
         id={props.imageBasemap ? props.imageBasemap.id : props.basemap.id}
         ref={mapRef}
