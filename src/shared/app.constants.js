e28a05e6efcdd22cdd0dd4d377e5bb2b5620e78d
NathanNovak <nnovak@ku.edu>
2021-06-29 14:17:02 -0700

    Versioned app to 1.3.3
    Signed-off-by: NathanNovak <nnovak@ku.edu>

--- src/shared/app.constants.js
+++ src/shared/app.constants.js
@@ -2,7 +2,7 @@ export const REDUX = {
   CLEAR_STORE: 'CLEAR_STORE',
 };
 
-// Be sure to change the version in X-Code to match TODO: Change X-Code version to match
-export const VERSION_NUMBER = '1.3.1';
+// Be sure to change the version in X-Code to match TODO: Change X-Code version and package.json to match!!
+export const VERSION_NUMBER = '1.3.3';
 export const BUNDLE_ID = 'org.StraboSpot2';
 export const RELEASE_NAME = `${BUNDLE_ID}-${VERSION_NUMBER}`;
