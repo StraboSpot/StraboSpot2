e1684c7941634d61c0f49aec37997f9ae4f0bd1b
Jessica Good <jessicagood012@gmail.com>
2021-07-15 12:39:15 -0700

    Pet Minerals: Added Look up in Glossary

--- src/modules/petrology/AddMineralModal.js
+++ src/modules/petrology/AddMineralModal.js
@@ -2,17 +2,19 @@ import React, {useEffect, useRef, useState} from 'react';
 import {FlatList, Platform, View} from 'react-native';
 
 import {Formik} from 'formik';
-import {Button} from 'react-native-elements';
+import {Button, ButtonGroup} from 'react-native-elements';
 import {useDispatch, useSelector} from 'react-redux';
 
-import {getNewId} from '../../shared/Helpers';
+import {getNewId, isEmpty} from '../../shared/Helpers';
 import SaveButton from '../../shared/SaveButton';
-import {PRIMARY_ACCENT_COLOR} from '../../shared/styles.constants';
+import {PRIMARY_ACCENT_COLOR, PRIMARY_TEXT_COLOR} from '../../shared/styles.constants';
 import DragAnimation from '../../shared/ui/DragAmination';
 import Modal from '../../shared/ui/modal/Modal';
 import {Form, useFormHook} from '../form';
 import {setModalValues} from '../home/home.slice';
 import {PAGE_KEYS} from '../page/page.constants';
+import MineralsByRockClass from './MineralsByRockClass';
+import MineralsGlossary from './MineralsGlossary';
 import usePetrologyHook from './usePetrology';
 
 const AddMineralModal = () => {
@@ -20,12 +22,14 @@ const AddMineralModal = () => {
   const spot = useSelector(state => state.spot.selectedSpot);
 
   const [choicesViewKey, setChoicesViewKey] = useState(null);
+  const [selectedTypeIndex, setSelectedTypeIndex] = useState(null);
 
   const formRef = useRef(null);
 
   const [useForm] = useFormHook();
   const usePetrology = usePetrologyHook();
 
+  const [initialValues, setInitialValues] = useState({id: getNewId()});
   const petKey = PAGE_KEYS.MINERALS;
   const formName = ['pet', petKey];
   const survey = useForm.getSurvey(formName);
@@ -34,30 +38,30 @@ const AddMineralModal = () => {
     return () => dispatch(setModalValues({}));
   }, []);
 
+  const addMineralFromGlossary = (mineralInfo) => {
+    setInitialValues({id: getNewId(), mineral_abbrev: mineralInfo.Abbreviation, full_mineral_name: mineralInfo.Label});
+    setSelectedTypeIndex(null);
+  };
+
+  const onViewTypePress = (i) => {
+    if (selectedTypeIndex === i) setSelectedTypeIndex(null);
+    else setSelectedTypeIndex(i);
+  };
+
   const renderAddMineralModalContent = () => {
     return (
       <Modal>
-        <React.Fragment>
-          <FlatList
-            bounces={false}
-            ListHeaderComponent={
-              <View style={{flex: 1}}>
-                <Formik
-                  innerRef={formRef}
-                  initialValues={{id: getNewId()}}
-                  onSubmit={(values) => console.log('Submitting form...', values)}
-                >
-                  {(formProps) => (
-                    <View style={{flex: 1}}>
-                      {choicesViewKey ? renderSubform(formProps) : renderForm(formProps)}
-                    </View>
-                  )}
-                </Formik>
-              </View>
-            }
-          />
-        </React.Fragment>
-        {choicesViewKey ? renderDoneButton() : renderSaveButton()}
+        <ButtonGroup
+          selectedIndex={selectedTypeIndex}
+          onPress={onViewTypePress}
+          buttons={['Look up by\nRock Class', 'Look up in\nGlossary']}
+          containerStyle={{height: 45, borderRadius: 10}}
+          buttonStyle={{padding: 5}}
+          textStyle={{color: PRIMARY_TEXT_COLOR}}
+        />
+        {isEmpty(selectedTypeIndex) && renderForms()}
+        {selectedTypeIndex === 0 && <MineralsByRockClass/>}
+        {selectedTypeIndex === 1 && <MineralsGlossary addMineral={addMineralFromGlossary}/>}
       </Modal>
     );
   };
@@ -81,6 +85,33 @@ const AddMineralModal = () => {
             ...formProps,
           }}
         />
+      </React.Fragment>
+    );
+  };
+
+  const renderForms = () => {
+    return (
+      <React.Fragment>
+        <FlatList
+          bounces={false}
+          ListHeaderComponent={
+            <View style={{flex: 1}}>
+              <Formik
+                innerRef={formRef}
+                initialValues={initialValues}
+                onSubmit={(values) => console.log('Submitting form...', values)}
+                enableReinitialize={true}
+              >
+                {(formProps) => (
+                  <View style={{flex: 1}}>
+                    {choicesViewKey ? renderSubform(formProps) : renderForm(formProps)}
+                  </View>
+                )}
+              </Formik>
+            </View>
+          }
+        />
+        {choicesViewKey ? renderDoneButton() : renderSaveButton()}
       </React.Fragment>
     );
   };
--- src/modules/petrology/MineralsGlossary.js
+++ src/modules/petrology/MineralsGlossary.js
@@ -5,7 +5,7 @@ import {Button} from 'react-native-elements';
 
 import {isEmpty} from '../../shared/Helpers';
 import * as themes from '../../shared/styles.constants';
-import SaveAndCloseButton from '../../shared/ui/SaveAndCloseButtons';
+import styles from '../page/ui/ui.styles';
 import {MINERAL_GLOSSARY_INFO} from './petrology.constants';
 
 const MineralsGlossary = (props) => {
@@ -22,63 +22,62 @@ const MineralsGlossary = (props) => {
   const renderMineralInfo = () => {
     return (
       <View>
-        <SaveAndCloseButton
-          cancel={() => setActiveMineralInfo({})}
-        />
-        <View style={{paddingLeft: 10, paddingRight: 10}}>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
           <Button
-            title='+ Add this Mineral'
+            titleStyle={styles.buttonText}
+            title={'Cancel'}
+            type={'clear'}
+            onPress={() => setActiveMineralInfo({})}
+          />
+          <Button
+            titleStyle={styles.buttonText}
+            title='Add Mineral'
             type={'clear'}
             onPress={() => props.addMineral(activeMineralInfo)}
           />
-          {Object.entries(activeMineralInfo).map(([field, value]) => {
-            if (field === 'mindat.org link') {
-              return (
-                <Button
-                  title={'Click here for more mineral information on ' + activeMineralInfo.Label + ' from Mindat.org'}
-                  type={'clear'}
-                  icon={{name: 'globe-outline', type: 'ionicon', color: themes.PRIMARY_ACCENT_COLOR}}
-                  containerStyle={{padding: 10}}
-                  onPress={() => Linking.openURL(value)}
-                />
-              );
-            }
-            else if (field !== 'Name') return <Text><Text style={{fontWeight: 'bold'}}>{field}:</Text> {value}</Text>;
-          })}
         </View>
+        {Object.entries(activeMineralInfo).map(([field, value]) => {
+          if (field === 'mindat.org link') {
+            return (
+              <Button
+                title={'Click here for more mineral information on ' + activeMineralInfo.Label + ' from Mindat.org'}
+                type={'clear'}
+                icon={{name: 'globe-outline', type: 'ionicon', color: themes.PRIMARY_ACCENT_COLOR}}
+                containerStyle={{padding: 10}}
+                onPress={() => Linking.openURL(value)}
+              />
+            );
+          }
+          else if (field !== 'Name') return <Text><Text style={{fontWeight: 'bold'}}>{field}:</Text> {value}</Text>;
+        })}
       </View>
     );
   };
 
   const renderMineralList = () => {
     return (
-      <View>
-        <SaveAndCloseButton
-          cancel={() => props.showMineralsOverview()}
-        />
-        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-evenly'}}>
-          {glossaryChunked.map((glossaryChunk) => (
-            <View style={{flex: 1}}>
-              {glossaryChunk.map(mineralInfo => (
-                <Button
-                  title={mineralInfo.Label}
-                  type={'clear'}
-                  onPress={() => setActiveMineralInfo(mineralInfo)}
-                />
-              ))}
-            </View>
-          ))}
-        </View>
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-evenly'}}>
+        {glossaryChunked.map((glossaryChunk) => (
+          <View style={{flex: 1}}>
+            {glossaryChunk.map(mineralInfo => (
+              <Button
+                title={mineralInfo.Label}
+                type={'clear'}
+                onPress={() => setActiveMineralInfo(mineralInfo)}
+              />
+            ))}
+          </View>
+        ))}
       </View>
     );
   };
 
   return (
-    <React.Fragment>
+    <View style={{paddingLeft: 10, paddingRight: 10}}>
       <FlatList ListHeaderComponent={
         isEmpty(activeMineralInfo) ? renderMineralList() : renderMineralInfo()
       }/>
-    </React.Fragment>
+    </View>
   );
 };
 
