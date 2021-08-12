4722b0de91f631d946e2a7f0bf96eaeaa1d12205
Jessica Good <jessicagood012@gmail.com>
2021-08-12 11:57:33 -0700

    Pet: Added Facies chart to Metamorphic Quick Entry Modal

--- src/modules/petrology/AddRockMetamorphicModal.js
+++ src/modules/petrology/AddRockMetamorphicModal.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
 
-import {Form, MainButtons} from '../form';
+import {Button} from 'react-native-elements';
+import {Overlay} from 'react-native-elements/dist/overlay/Overlay';
+
+import {isEmpty} from '../../shared/Helpers';
+import * as themes from '../../shared/styles.constants';
+import {REACT_NATIVE_ELEMENTS_BLUE, SECONDARY_BACKGROUND_COLOR} from '../../shared/styles.constants';
+import modalStyle from '../../shared/ui/modal/modal.style';
+import {Form, formStyles, MainButtons, useFormHook} from '../form';
 
 const AddRockMetamorphicModal = (props) => {
+  const [isFaciesModalVisible, setIsFaciesModalVisible] = useState(false);
+
+  const [useForm] = useFormHook();
+
   // Relevant keys for quick-entry modal
   const firstKeys = ['metamorphic_rock_type'];
   const secondKeys = ['protolith'];
@@ -12,6 +24,131 @@ const AddRockMetamorphicModal = (props) => {
 
   // Relevant fields for quick-entry modal
   const lastKeysFields = lastKeys.map(k => props.survey.find(f => f.name === k));
+
+  const addFacies = (faciesPressed) => {
+    const currentFacies = JSON.parse(JSON.stringify(props.formRef.current.values?.facies || []));
+    const updatedFacies = currentFacies.includes(faciesPressed) ? currentFacies.filter(f => f !== faciesPressed)
+      : [...currentFacies, faciesPressed];
+    if (isEmpty(updatedFacies)) {
+      const {facies, ...valuesNew} = props.formRef.current.values;
+      props.formRef.current?.setValues(valuesNew);
+    }
+    else props.formRef.current.setFieldValue('facies', updatedFacies);
+  };
+
+  const renderModalHeader = () => {
+    return (
+      <View style={modalStyle.modalTop}>
+        <View style={{flex: 1, alignItems: 'flex-start'}}/>
+        <View>
+          <Text style={modalStyle.modalTitle}>Facies</Text>
+        </View>
+        <View style={{flex: 1, alignItems: 'flex-end'}}>
+          <Button
+            titleStyle={{color: themes.PRIMARY_ACCENT_COLOR, fontSize: 16}}
+            title={'Done'}
+            type={'clear'}
+            onPress={() => setIsFaciesModalVisible(false)}
+          />
+        </View>
+      </View>
+    );
+  };
+
+  const faciesButtonText = (key) => {
+    return (
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <Text
+          style={props.formRef.current?.values?.facies?.includes(key) ? formStyles.formButtonSelectedTitle
+            : formStyles.formButtonTitle}>
+          {useForm.getLabel(key, props.formName)}
+        </Text>
+      </View>
+    );
+  };
+
+  const FaciesButton = (faciesProps) => {
+    return (
+      <Button
+        containerStyle={{padding: 2.5}}
+        buttonStyle={[formStyles.formButtonLarge, {
+          backgroundColor: props.formRef.current?.values?.facies?.includes(faciesProps.faciesKey)
+            ? REACT_NATIVE_ELEMENTS_BLUE : SECONDARY_BACKGROUND_COLOR,
+          height: '100%',
+        }]}
+        type={'outline'}
+        title={() => faciesButtonText(faciesProps.faciesKey)}
+        onPress={() => addFacies(faciesProps.faciesKey)}
+      />
+    );
+  };
+
+  const renderFaciesModal = () => {
+    return (
+      <Overlay overlayStyle={{width: '80%', height: '80%'}} isVisible={true}>
+        {renderModalHeader()}
+        <View style={{flex: 1}}>
+          <View style={{flex: 5, flexDirection: 'row', paddingRight: 5}}>
+            <View style={{width: 20, alignItems: 'center', justifyContent: 'center'}}>
+              <Text style={[formStyles.formButtonTitle, {transform: [{rotate: '-90deg'}], width: 60}]}>Pressure</Text>
+            </View>
+            <View style={{
+              flex: 1,
+              flexDirection: 'column',
+              borderStyle: 'solid',
+              borderLeftWidth: 1,
+              borderBottomWidth: 1,
+            }}>
+              <View style={{flex: 8, flexDirection: 'row'}}>
+                <View style={{flex: 9, flexDirection: 'column'}}>
+                  <View style={{flex: 2, flexDirection: 'row'}}>
+                    <View style={{flex: 4}}/>
+                    <View style={{flex: 5}}>
+                      <FaciesButton faciesKey={'eclogite'}/>
+                    </View>
+                  </View>
+                  <View style={{flex: 4, flexDirection: 'row'}}>
+                    <View style={{flex: 5, flexDirection: 'column'}}>
+                      <View style={{flex: 2, flexDirection: 'row'}}>
+                        <View style={{flex: 2}}/>
+                        <View style={{flex: 2}}><FaciesButton faciesKey={'blueschist'}/></View>
+                        <View style={{flex: 1}}/>
+                      </View>
+                      <View style={{flex: 2, flexDirection: 'row'}}>
+                        <View style={{flex: 1}}/>
+                        <View style={{flex: 2}}><FaciesButton faciesKey={'prehnite_pumpy'}/></View>
+                        <View style={{flex: 2}}><FaciesButton faciesKey={'greenschist'}/></View>
+                      </View>
+                    </View>
+                    <View style={{flex: 2}}><FaciesButton faciesKey={'amphibolite'}/></View>
+                    <View style={{flex: 2}}><FaciesButton faciesKey={'upper_amph'}/></View>
+                  </View>
+                </View>
+                <View style={{flex: 2}}><FaciesButton faciesKey={'granulite'}/></View>
+              </View>
+              <View style={{flex: 1, flexDirection: 'row'}}>
+                <View style={{flex: 2}}><FaciesButton faciesKey={'zeolite'}/></View>
+                <View style={{flex: 2}}/>
+                <View style={{flex: 5}}><FaciesButton faciesKey={'hornfels'}/></View>
+                <View style={{flex: 2}}/>
+              </View>
+            </View>
+          </View>
+          <View style={{height: 30, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={formStyles.formButtonTitle}>Temperature</Text>
+          </View>
+          <View style={{flexDirection: 'row', height: 80}}>
+            <View style={{width: '50%'}}>
+              <FaciesButton faciesKey={'ultra_high_pre'}/>
+            </View>
+            <View style={{width: '50%'}}>
+              <FaciesButton faciesKey={'ultra_high_tem'}/>
+            </View>
+          </View>
+        </View>
+      </Overlay>
+    );
+  };
 
   return (
     <React.Fragment>
@@ -31,7 +168,7 @@ const AddRockMetamorphicModal = (props) => {
         mainKeys: thirdKeys,
         formName: props.formName,
         formRef: props.formRef,
-        setChoicesViewKey: props.setChoicesViewKey,
+        setChoicesViewKey: () => setIsFaciesModalVisible(true),
       }}/>
       <MainButtons {...{
         mainKeys: fourthKeys,
@@ -43,6 +180,7 @@ const AddRockMetamorphicModal = (props) => {
         surveyFragment: lastKeysFields,
         ...props.formProps,
       }}/>
+      {isFaciesModalVisible && renderFaciesModal()}
     </React.Fragment>
   );
 };
