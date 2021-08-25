778abd5963c8ef1d57d783af2ff05e1e3648462b
Nathan Novak <nnovak@ku.edu>
2021-08-25 11:26:51 -0700

    Added ability for the android navigation bar to hide when app is focused.  User able to access it by swiping up on device.

--- android/app/src/main/java/com/strabospot2/MainActivity.java
+++ android/app/src/main/java/com/strabospot2/MainActivity.java
@@ -4,6 +4,8 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
  import com.facebook.react.ReactRootView;
  import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+ import android.os.Bundle;
+ import android.view.View;
 
 public class MainActivity extends ReactActivity {
 
@@ -24,4 +26,23 @@ public class MainActivity extends ReactActivity {
         }
       };
     }
+     @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            hideNavigationBar();
+        }
+
+        @Override
+        public void onWindowFocusChanged(boolean hasFocus) {
+            super.onWindowFocusChanged(hasFocus);
+            if (hasFocus) {
+                hideNavigationBar();
+            }
+        }
+
+        private void hideNavigationBar() {
+            getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
 }
