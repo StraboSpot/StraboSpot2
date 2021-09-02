7d55efbfc3ea33dfb4fc8abd1b0ffa82f9fccec7
nathannovak <nnovak@ku.edu>
2021-09-02 12:07:34 -0700

    Main.Bundle: changed main.bundle path Versioned app to 1.8.0

--- src/shared/app.constants.js
+++ src/shared/app.constants.js
@@ -3,6 +3,6 @@ export const REDUX = {
 };
 
 // Be sure to change the version in X-Code to match TODO: Change X-Code version and package.json to match!!
-export const VERSION_NUMBER = '1.7.3';
+export const VERSION_NUMBER = '1.8.0';
 export const BUNDLE_ID = 'org.StraboSpot2';
 export const RELEASE_NAME = `${BUNDLE_ID}-${VERSION_NUMBER}`;
