38f843d01d1b22fd918211c733f1322e3ba72fb1
nathannovak <nnovak@ku.edu>
2021-10-11 14:31:18 -0700

    Documatation: Added ability to view .pdf docs that are loaded as assets

--- src/modules/main-menu-panel/Documentation.js
+++ src/modules/main-menu-panel/Documentation.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import {Alert, FlatList, Linking, ListView, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Alert, FlatList, Linking, View} from 'react-native';
 
-import {ListItem} from 'react-native-elements';
+import {Button, Icon, ListItem, Overlay} from 'react-native-elements';
+import {WebView} from 'react-native-webview';
+import {useSelector} from 'react-redux';
 
 import commonStyles from '../../shared/common.styles';
+import {BLACK, BLUE} from '../../shared/styles.constants';
 import FlatListItemSeparator from '../../shared/ui/FlatListItemSeparator';
 import SectionDivider from '../../shared/ui/SectionDivider';
+import styles from './documentation.styles';
 
-const Documentation = () => {
+const Documentation = (props) => {
+  const isOnline = useSelector(state => state.home.isOnline.isInternetReachable);
 
+  const [visible, setVisible] = useState(false);
+  const [doc, setDoc] = useState('');
+
+  const helpUrl = 'https://strabospot.org/help';
   const files = [
     {
       name: 'How to Airdrop backup files to other iPads',
-      path: 'https://strabospot.org/files/helpFiles/Airdrop_from_iPad_to_iPad.pdf',
+      pdf: require('../../assets/documents/AirdropFromIPadToIPad.pdf'),
     },
     {
       name: 'Moving backups out of StraboSpot 2 folder ',
-      path: 'https://strabospot.org/files/helpFiles/Moving_Project_Backups_Out_of%20StraboSpot2.pdf',
-    },
-    {
-      name: 'Other Strabo Help Docs ',
-      path: 'https://strabospot.org/help',
+      pdf: require('../../assets/documents/MovingProjectBackupsOutOfStraboSpot2.pdf'),
     },
   ];
 
-  const viewFile = async (path) => {
+  const toggleOverlay = (pdf) => {
+    setDoc(pdf);
+    setVisible(!visible);
+  };
+
+  const viewOnlineHelp = async (path) => {
     try {
       const canOpen = await Linking.canOpenURL(path);
       canOpen && await Linking.openURL(path);
@@ -35,8 +45,29 @@ const Documentation = () => {
     }
   };
 
+  const viewPDF = () => {
+    return (
+      <Overlay isVisible={visible} onBackdropPress={toggleOverlay} overlayStyle={styles.overlayContainer}>
+        <Button
+          type={'clear'}
+          containerStyle={{alignItems: 'flex-end'}}
+          onPress={() => setVisible(!visible)}
+          icon={
+            <Icon
+              name='close-outline'
+              type={'ionicon'}
+              size={30}
+              color={BLACK}
+            />
+          }
+        />
+        <WebView source={doc}/>
+      </Overlay>
+    );
+  };
+
   const renderFAQListItem = ({item}) => (
-    <ListItem key={item} onPress={() => viewFile(item.path)} containerStyle={commonStyles.listItem}>
+    <ListItem key={item} onPress={() => toggleOverlay(item.pdf)} containerStyle={commonStyles.listItem}>
       <ListItem.Title>{item.name}</ListItem.Title>
     </ListItem>
   );
@@ -50,14 +81,31 @@ const Documentation = () => {
     />
   );
 
+  const renderHelpLink = () => (
+    <View style={styles.bottomButton}>
+      {isOnline && <Button
+        title={'StraboSpot Help Center'}
+        type={'clear'}
+        onPress={() => viewOnlineHelp(helpUrl)}
+        icon={
+          <Icon
+            name='globe-outline'
+            type={'ionicon'}
+            iconStyle={{paddingRight: 10}}
+            size={20}
+            color={BLUE}
+          />
+        }
+      />}
+    </View>
+  );
+
   return (
     <View style={{flex: 1}}>
-      <View>
-        <SectionDivider dividerText={'FAQ\'s'}/>
-        {renderFAQitems()}
-      </View>
-      <View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center', marginBottom: 15}}>
-      </View>
+      <SectionDivider dividerText={'FAQ\'s'}/>
+      {renderFAQitems()}
+      {renderHelpLink()}
+      {viewPDF()}
     </View>
   );
 };
---
+++ src/modules/main-menu-panel/documentation.styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+const documentationStyles = StyleSheet.create({
+  activityIndicatorStyle: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  bottomButton: {
+    justifyContent: 'flex-end',
+    margin: 20,
+  },
+  overlayContainer: {
+    width: '75%',
+    height: '100%',
+  },
+});
+
+export default documentationStyles;
