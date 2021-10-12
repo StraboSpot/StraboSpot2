9fc8fec2c9f4cd1793fa5d5dd8b6a55f77f7a792
nathannovak <nnovak@ku.edu>
2021-10-12 16:35:15 -0700

    Documentation: Added PDFView to open pdfs on iOS.

--- src/modules/main-menu-panel/Documentation.js
+++ src/modules/main-menu-panel/Documentation.js
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
-import {Alert, FlatList, Linking, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Alert, FlatList, Linking, Platform, View} from 'react-native';
 
 import {Button, Icon, ListItem, Overlay} from 'react-native-elements';
-import {WebView} from 'react-native-webview';
+import PDFView from 'react-native-view-pdf';
 import {useSelector} from 'react-redux';
 
 import commonStyles from '../../shared/common.styles';
@@ -12,26 +12,31 @@ import SectionDivider from '../../shared/ui/SectionDivider';
 import styles from './documentation.styles';
 
 const Documentation = (props) => {
+
   const isOnline = useSelector(state => state.home.isOnline.isInternetReachable);
 
   const [visible, setVisible] = useState(false);
   const [doc, setDoc] = useState('');
 
   const helpUrl = 'https://strabospot.org/help';
+
   const files = [
     {
+      label: 'airdrop',
       name: 'How to Airdrop backup files to other iPads',
-      pdf: require('../../assets/documents/AirdropFromIPadToIPad.pdf'),
+      file: Platform.OS === 'ios' ? 'Airdrop-from-iPad-to-iPad.pdf' : '/sdcard/Download/Airdrop-from-iPad-to-iPad.pdf',
     },
     {
+      label: 'moveFiles',
       name: 'Moving backups out of StraboSpot 2 folder ',
-      pdf: require('../../assets/documents/MovingProjectBackupsOutOfStraboSpot2.pdf'),
+      file: Platform.OS === 'ios' ? 'Moving-Project-Backups-Out-of-StraboSpot2.pdf' : '/sdcard/Download/Moving-Project-Backups-Out-of-StraboSpot2.pdf',
     },
   ];
 
-  const toggleOverlay = (pdf) => {
-    setDoc(pdf);
-    setVisible(!visible);
+  const toggleOverlay = (pdfLabel) => {
+    setDoc(pdfLabel);
+    Platform.OS === 'ios' ? setVisible(!visible) : Alert.alert('Currently not available on Android', `Visit ${helpUrl}`);
+    // setVisible(!visible);
   };
 
   const viewOnlineHelp = async (path) => {
@@ -45,7 +50,16 @@ const Documentation = (props) => {
     }
   };
 
+  const findFile = () => {
+    // let file;
+    const file = files.find((pdf, index) =>  {
+      if (pdf.label === doc) return pdf.file;
+    });
+    return file &&  file.file;
+  };
+
   const viewPDF = () => {
+    const resourceType = 'file';
     return (
       <Overlay isVisible={visible} onBackdropPress={toggleOverlay} overlayStyle={styles.overlayContainer}>
         <Button
@@ -54,20 +68,28 @@ const Documentation = (props) => {
           onPress={() => setVisible(!visible)}
           icon={
             <Icon
-              name='close-outline'
+              name={'close-outline'}
               type={'ionicon'}
               size={30}
               color={BLACK}
             />
           }
         />
-        <WebView source={doc}/>
+        {/*<WebView source={doc}/>*/}
+        <PDFView
+          fadeInDuration={250.0}
+          style={{ flex: 1}}
+          resource={findFile()}
+          resourceType={resourceType}
+          onLoad={() => console.log(`PDF rendered from ${resourceType}`)}
+          onError={(error) => console.log('Cannot render PDF', error)}
+        />
       </Overlay>
     );
   };
 
   const renderFAQListItem = ({item}) => (
-    <ListItem key={item} onPress={() => toggleOverlay(item.pdf)} containerStyle={commonStyles.listItem}>
+    <ListItem key={item} onPress={() => toggleOverlay(item.label)} containerStyle={commonStyles.listItem}>
       <ListItem.Title>{item.name}</ListItem.Title>
     </ListItem>
   );
@@ -83,20 +105,22 @@ const Documentation = (props) => {
 
   const renderHelpLink = () => (
     <View style={styles.bottomButton}>
-      {isOnline && <Button
+      {isOnline && (
+        <Button
         title={'StraboSpot Help Center'}
         type={'clear'}
         onPress={() => viewOnlineHelp(helpUrl)}
         icon={
           <Icon
-            name='globe-outline'
+            name={'globe-outline'}
             type={'ionicon'}
             iconStyle={{paddingRight: 10}}
             size={20}
             color={BLUE}
           />
         }
-      />}
+      />
+      )}
     </View>
   );
 
