e1b6a7af4eb9d9c78bcf38c9aeda35c9ee5f344c
Nathan Novak <nnovak@ku.edu>
2021-10-21 15:51:41 -0700

    Adjusted font for iOS

--- src/modules/petrology/AddMineralModal.js
+++ src/modules/petrology/AddMineralModal.js
@@ -7,7 +7,7 @@ import {useDispatch, useSelector} from 'react-redux';
 
 import {getNewId, isEmpty} from '../../shared/Helpers';
 import SaveButton from '../../shared/SaveButton';
-import {PRIMARY_TEXT_COLOR} from '../../shared/styles.constants';
+import {PRIMARY_TEXT_COLOR, SMALL_TEXT_SIZE} from '../../shared/styles.constants';
 import DragAnimation from '../../shared/ui/DragAmination';
 import LittleSpacer from '../../shared/ui/LittleSpacer';
 import Modal from '../../shared/ui/modal/Modal';
@@ -96,10 +96,10 @@ const AddMineralModal = (props) => {
           <ButtonGroup
             selectedIndex={selectedTypeIndex}
             onPress={onViewTypePress}
-            buttons={['Look up by\nRock Class', 'Look up in\nGlossary']}
-            containerStyle={{height: 45, borderRadius: 10}}
+            buttons={['Look up by Rock Class', 'Look up in Glossary']}
+            containerStyle={{height: 50, borderRadius: 10}}
             buttonStyle={{padding: 5}}
-            textStyle={{color: PRIMARY_TEXT_COLOR}}
+            textStyle={{color: PRIMARY_TEXT_COLOR, fontSize: SMALL_TEXT_SIZE, textAlign: 'center'}}
           />
         )}
         {isEmpty(selectedTypeIndex) && renderForms()}
