f13de3d04113ec24b32fa0cce71abf6a4ee03fa2
Jessica Good <jessicagood012@gmail.com>
2022-01-26 15:54:59 -0700

    Fabrics: Forgot to remove solid_state_str

--- src/modules/fabrics/fabric.constants.js
+++ src/modules/fabrics/fabric.constants.js
@@ -6,6 +6,6 @@ export const FABRIC_TYPES = {
 
 export const FIRST_ORDER_FABRIC_FIELDS = {
   fault_rock: ['structural_fabric', 'kinematic_fab'],
-  igneous_rock: ['planar_fab', 'lin_fab', 'magmatic_str', 'solid_state_str'],
+  igneous_rock: ['planar_fab', 'lin_fab', 'magmatic_str'],
   metamorphic_rock: ['planar_fabric', 'linear_fab', 'other_met_fab', 'kinematic_ind'],
 };
