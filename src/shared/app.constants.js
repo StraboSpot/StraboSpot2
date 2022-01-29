d6bcf0108be06ae3043aced2318fd51cf6e81451
Nathan Novak <nnovak@ku.edu>
2022-01-28 17:12:19 -0700

    Versioned to 1.17.1

--- src/shared/app.constants.js
+++ src/shared/app.constants.js
@@ -4,5 +4,5 @@ export const REDUX = {
 
 // Be sure to change the version in X-Code to match TODO: Change X-Code version and package.json to match!!
 export const BUNDLE_ID = 'org.StraboSpot2';
-export const VERSION_NUMBER = '1.17.0';
+export const VERSION_NUMBER = '1.17.1';
 export const RELEASE_NAME = `${BUNDLE_ID}-${VERSION_NUMBER}`;
