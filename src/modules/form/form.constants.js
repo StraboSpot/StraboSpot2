ed7cb03ddebb09888d26b51cdd56d59887f56e18
Jessica Good <jessicagood012@gmail.com>
2022-03-17 12:13:00 -0700

    Label Dictionary: Fixed so would still make a dictionary for forms that do not have Choices

--- src/modules/form/form.constants.js
+++ src/modules/form/form.constants.js
@@ -4,14 +4,11 @@ import * as forms from '../../assets/forms';
 // each choice value across all forms organized by form
 export const LABEL_DICTIONARY = Object.keys(forms.default).reduce((acc, key) => {
   const res = Object.keys(forms.default[key]).reduce((acc1, key1) => {
-    if (forms.default[key][key1].survey && forms.default[key][key1].choices) {
-      const surveyLabels1 = Object.assign({},
-        ...forms.default[key][key1].survey.map(item => ({[item.name]: item.label})));
-      const choicesLabels1 = Object.assign({},
-        ...forms.default[key][key1].choices.map(item => ({[item.name]: item.label})));
-      return {...acc1, [key1]: {...surveyLabels1, ...choicesLabels1}};
-    }
-    else return acc1;
+    const surveyLabels1 = forms.default[key][key1].survey && Object.assign({},
+      ...forms.default[key][key1].survey.map(item => ({[item.name]: item.label})));
+    const choicesLabels1 = forms.default[key][key1].choices && Object.assign({},
+      ...forms.default[key][key1].choices.map(item => ({[item.name]: item.label})));
+    return {...acc1, [key1]: {...surveyLabels1, ...choicesLabels1}};
   }, {});
   return {...acc, [key]: res};
 }, {});
