10eb3cdcbf6897eeed4d5dd22c7e1a129c6eea2f
Nathan Novak <nnovak@ku.edu>
2022-10-04 12:56:36 -0700

    feat(Splashscreen.js): Added Splashscreen.js to project and moved the background image to it.  Changed the background image.
    Added styling to make more responsive on tablets and phones.

---
+++ src/modules/splashscreen/Splashscreen.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {ImageBackground, Platform, Text, useWindowDimensions, View} from 'react-native';
+
+import BatteryInfo from '../../services/BatteryInfo';
+import ConnectionStatus from '../../services/ConnectionStatus';
+import {VERSION_NUMBER} from '../../shared/app.constants';
+import {getFontSizeByWindowWidth} from '../../shared/Helpers';
+import splashscreenStyles from './splashscreen.styles';
+
+const Splashscreen = (props) => {
+  const windowDimensions = useWindowDimensions();
+
+  const fontSize = getFontSizeByWindowWidth(windowDimensions, 40);
+  const titleStyles = [splashscreenStyles.title, {fontSize}];
+
+
+  return (
+    <ImageBackground
+      source={require('../../assets/images/splashscreen-1.jpeg')}
+      style={splashscreenStyles.backgroundImage}
+    >
+      <View style={splashscreenStyles.wifiIndicatorContainer}>
+        {Platform.OS === 'ios' && <BatteryInfo/>}
+        <ConnectionStatus/>
+      </View>
+      <View style={splashscreenStyles.contentContainer}>
+        <View style={splashscreenStyles.titleContainer}>
+          <Text style={titleStyles}>Strabo Spot 2</Text>
+        </View>
+        {props.children}
+      </View>
+      <Text style={splashscreenStyles.version}>v{VERSION_NUMBER}</Text>
+    </ImageBackground>
+  );
+};
+
+export default Splashscreen;
