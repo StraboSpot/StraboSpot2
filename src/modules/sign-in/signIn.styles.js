4aab0b5d8f0f153c4d7556fd085d824a039ae173
Nathan Novak <nnovak@ku.edu>
2022-10-04 12:47:50 -0700

    feat(SignIn.js): Added SplashScreen Component and changed View to Animated.View. Removed button icons and moved container styling to signIn.styles.js

--- src/modules/sign-in/signIn.styles.js
+++ src/modules/sign-in/signIn.styles.js
@@ -7,42 +7,35 @@ const styles = StyleSheet.create({
     width: 350,
     fontSize: themes.PRIMARY_TEXT_SIZE,
     fontWeight: '500',
-    height: 55,
+    height: 40,
     backgroundColor: themes.SECONDARY_BACKGROUND_COLOR,
     margin: 10,
     color: 'black',
     padding: 8,
     borderRadius: 14,
   },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'cover',
-  },
   signInContainer: {
     alignItems: 'center',
   },
-  titleContainer: {
-    paddingBottom: 75,
-  },
-  title: {
-    fontSize: 110,
-    fontFamily: 'Arial-BoldMT',
-  },
   version: {
-    // color: 'black',
     textAlign: 'center',
     fontSize: 32,
     fontFamily: 'ChalkboardSE-Bold',
   },
+  buttonContainer: {
+    margin: 10,
+  },
+  buttonsContainer: {
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   buttonStyle: {
     borderRadius: 30,
-    paddingRight: 50,
-    paddingLeft: 50,
+    paddingRight: 20,
+    paddingLeft: 20,
   },
 });
 
