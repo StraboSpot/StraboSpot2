697eb15d584bd4ffecd43119f0f5698c9ef2b37d
Jessica Good <jessicagood012@gmail.com>
2022-11-07 12:52:28 -0700

    build(Update RN to 0.69.0):

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ LogBox.ignoreLogs([
   'Sending `zipArchiveProgressEvent` with no listeners registered.',
   'currentlyFocusedField is deprecated and will be removed in a future release. Use currentlyFocusedInput',
   'Mapbox error You\'re calling ',
-  'ViewPropTypes',
 ]);
 
 AppRegistry.registerComponent(appName, () => App);
