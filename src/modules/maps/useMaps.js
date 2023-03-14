3ba7cbb04fb8a3ba4244ee3ed6a2a3a74285ec02
Jessica Good <jessicagood012@gmail.com>
2023-03-14 13:05:31 -0700

    fix(map): Returned geolocate accuracy to High
    Seems to work with Android and not timeout

--- src/modules/maps/useMaps.js
+++ src/modules/maps/useMaps.js
@@ -233,8 +233,7 @@ const useMaps = (mapRef) => {
 
   // Get the current location from the device and set it in the state
   const getCurrentLocation = async () => {
-    const geolocationOptions = {timeout: 5000, maximumAge: 10000, enableHighAccuracy: Platform.OS === 'ios'};
-    // Issue with Android not getting current location if enableHighAccuracy is true
+    const geolocationOptions = {timeout: 5000, maximumAge: 10000, enableHighAccuracy: true};
     return (
       new Promise((resolve, reject) => {
         Geolocation.getCurrentPosition(
