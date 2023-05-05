98beebe06f840f32e31a1f3f89e4e16be816d12b
Nathan Novak <nnovak@ku.edu>
2023-05-05 08:31:52 -0700

    build(package.json): Re-added Why-Did-You-Render to project

--- wdyr.js
+++ wdyr.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 if (process.env.NODE_ENV === 'development') {
-  const whyDidYouRender = require("@welldone-software/why-did-you-render");
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
-    trackAllPureComponents: true,
+    trackAllPureComponents: false,
     collapseGroups: true,
   });
 }
