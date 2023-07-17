82baaa23d3a687af41b62d98f750d2439a4b20ed
Jessica Good <jessicagood012@gmail.com>
2023-07-17 14:42:02 -0700

    refactor(notebook): Use value prop instead of defaultValue for TextInput in header

--- src/modules/notebook-panel/notebook-header/NotebookHeader.js
+++ src/modules/notebook-panel/notebook-header/NotebookHeader.js
@@ -167,9 +167,10 @@ const NotebookHeader = (props) => {
       />
       <View style={headerStyles.headerSpotNameAndCoordsContainer}>
         <TextInput
-          defaultValue={spot.properties.name}
+          value={spot.properties.name}
           onChangeText={text => onSpotEdit('name', text)}
-          style={headerStyles.headerSpotName}/>
+          style={headerStyles.headerSpotName}
+        />
         {getSpotCoordText() ? renderCoordsText() : renderSetCoordsText()}
       </View>
       <View>
