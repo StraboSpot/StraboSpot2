879bdf32d6d0606512acd44b100efbb7ddf50caf
Jessica Good <jessicagood012@gmail.com>
2023-07-19 14:22:41 -0700

    style(style): Formatting changes

--- src/shared/Helpers.js
+++ src/shared/Helpers.js
@@ -266,9 +266,8 @@ export const isEmpty = (value) => {
 };
 
 export const isEqual = (a, b) => {
-
   const isEqual = lodashIsEqual(a, b);
-  console.log(isEqual)
+  // console.log('isEqual' isEqual);
   return isEqual;
 };
 
@@ -357,5 +356,3 @@ const notEmptyValidator = (val) => {
   console.log(val);
   return val.trim() !== '';
 };
-
-
