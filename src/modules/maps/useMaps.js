a27ebfbc259b5db093821a86702705f81c1cd625
Jessica Good <jessicagood012@gmail.com>
2023-08-10 14:37:16 -0700

    fix(map): Fixed getNearestFeatureInBBox to work with Android after Mapbox v10 updated and refactored

--- src/modules/maps/useMaps.js
+++ src/modules/maps/useMaps.js
@@ -300,7 +300,8 @@ const useMaps = (mapRef) => {
   // Get the nearest draw feature from the draw layer where the screen was pressed
   const getDrawFeatureAtPress = async (screenPointX, screenPoint) => {
     const nearestDrawFeature = await getNearestFeatureInBBox([screenPointX, screenPoint], ['pointLayerDraw']);
-    console.log('Got draw feature: ', nearestDrawFeature);
+    if (isEmpty(nearestDrawFeature)) console.log('No draw features near press.');
+    else console.log('Got draw feature:', nearestDrawFeature);
     return Promise.resolve(nearestDrawFeature);
   };
 
@@ -311,12 +312,12 @@ const useMaps = (mapRef) => {
     return url + '?extent=' + extentString + '&zoom=' + zoomLevel;
   };
 
-  // Get the nearest feature to a target point within a bounding box from given layers
+  // Get the nearest feature to a target point in screen coordinates within a bounding box from given layers
   const getNearestFeatureInBBox = async ([x, y], layers) => {
 
-    // Get a bounding box for a point
+    // Get a bounding box for a pressed point on screen [top, right, bottom, left]
     const getBoundingBox = () => {
-      const r = 30;
+      const r = 15;
       const maxX = x + r;
       const minX = x - r;
       const maxY = y + r;
@@ -325,54 +326,28 @@ const useMaps = (mapRef) => {
     };
 
     // Get all the features in the bounding box
-    // Note: In RNMapbox v10.0.3 queryRenderedFeaturesInRect is returning all features in
-    // the visible bound of the map for Android so there is a temporary fix in the next code block
-    // ToDo: Check to see if queryRenderedFeaturesInRect is fixed for Android with updated RNMapbox versions
     const bbox = getBoundingBox();
     const nearFeaturesCollection = await mapRef.current.queryRenderedFeaturesInRect(bbox, null, layers);
-    const nearFeatures = nearFeaturesCollection.features;
-    console.log('Near Features:', nearFeatures);
+    let nearFeatures = nearFeaturesCollection.features;
 
-    // Get the target coordinates as a point with geographic coordinates
-    let targetCoordinates = await mapRef.current.getCoordinateFromView([x, y]);
-    const targetPoint = turf.point(targetCoordinates);
+    // ToDo: In RNMapbox v10.0.3 queryRenderedFeaturesInRect is returning all features in the visible map bounds for
+    //  Android only. Check to see if queryRenderedFeaturesInRect is fixed for Android with updated RNMapbox versions
+    // Android fix for queryRenderedFeaturesInRect returning all features in view
+    // Create a polygon from the bounding box and get features that intersect it
+    if (Platform.OS === 'android') {
+      const turfBbox = bbox.slice().reverse(); //turf bbox is minX, minY, maxX, maxY
+      const polyBbox = turf.bboxPolygon(turfBbox);
+      const geoPolyBbox = await Promise.all(
+        polyBbox.geometry.coordinates[0].map(async c => await mapRef.current.getCoordinateFromView(c)));
+      const poly = turf.polygon([geoPolyBbox]);
+      nearFeatures = nearFeaturesCollection.features.reduce(
+        (acc, nearFeature) => turf.booleanIntersects(nearFeature, poly) ? [...acc, nearFeature] : acc, []);
+    }
+    if (nearFeatures.length > 0) console.log('Near features:', nearFeatures);
 
-    // Get the nearest feature to the target point
-    const {nearestFeature} = nearFeatures.reduce((acc, nearFeature) => {
-      if (nearFeature.geometry.type === 'Point' || nearFeature.geometry.type === 'MultiPoint') {
-        const nearVertices = turf.explode(nearFeature);
-        const nearestVertex = turf.nearestPoint(targetPoint, nearVertices);
-        const distance = nearestVertex.properties.distanceToPoint;
-        // ToDo: In RNMapbox v10.0.3 queryRenderedFeaturesInRect is returning everything in the viewport so use this until fixed
-        if (Platform.OS === 'android') {
-          if (distance < .2 && (isEmpty(acc) || distance < acc.distance)) {
-            return {nearestFeature: nearFeature, distance: distance};
-          }
-        }
-        else if (isEmpty(acc) || distance < acc.distance) return {nearestFeature: nearFeature, distance: distance};
-      }
-      else {
-        const nearLines = (nearFeature.geometry.type === 'Polygon' || nearFeature.geometry.type === 'MultiPolygon')
-          ? turf.flatten(turf.polygonToLine(nearFeature))
-          : turf.flatten(nearFeature);
-        const nearestLine = nearLines.features.reduce((acc2, nearLine) => {
-          const nearestPointOnLine = turf.nearestPointOnLine(nearLine, targetPoint);
-          const distance = nearestPointOnLine.properties.dist;
-          // ToDo: In RNMapbox v10.0.3 queryRenderedFeaturesInRect is returning everything in the viewport so use this until fixed
-          if (Platform.OS === 'android') {
-            if (distance < .10 && (isEmpty(acc) || distance < acc.distance)) {
-              return {nearestFeature: nearFeature, distance: distance};
-            }
-          }
-          else if (isEmpty(acc) || distance < acc.distance) return {nearestFeature: nearFeature, distance: distance};
-          return acc2;
-        }, {});
-        if (!isEmpty(nearestLine)) return nearestLine;
-      }
-      return acc;
-    }, {});
-    console.log('Got nearest feature: ', nearestFeature);
-    return Promise.resolve(nearestFeature);
+    // If more than one near feature is found, return a random one (user needs to zoom in if too many features found)
+    const randomIndex = Math.floor(Math.random() * nearFeatures.length);
+    return Promise.resolve(nearFeatures[randomIndex] || []);
   };
 
   const getMeasureFeatures = async (e, measureFeaturesTemp, setDistance) => {
@@ -434,7 +409,8 @@ const useMaps = (mapRef) => {
     const nearestFeature = await getNearestFeatureInBBox([screenPointX, screenPointY], spotLayers);
     const nearestSpot = nearestFeature?.properties?.id ? useSpots.getSpotById(nearestFeature.properties.id)
       : {};
-    console.log('Got nearest spot: ', nearestSpot);
+    if (isEmpty(nearestSpot)) console.log('No spots near press.');
+    else console.log('Got nearest spot:', nearestSpot);
     return Promise.resolve(...[nearestSpot]);
   };
 
