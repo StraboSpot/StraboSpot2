203944534a2f9ff215c01bd815eb333492dd20f3
Jessica Good <jessicagood012@gmail.com>
2023-09-12 15:45:29 -0700

    build(packages): Eslint and Prettier
    Other related packages updated too

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,10 @@
 module.exports = {
-  root: true,
   env: {
     'browser': true,
     'es2021': true,
     'react-native/react-native': true,
   },
-  extends: ['@react-native', 'eslint:recommended', 'plugin:react/recommended', 'plugin:import/recommended'],
+  extends: ['@react-native', 'plugin:import/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -13,20 +12,23 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', 'react-hooks', 'react-native'],
+  root: true,
   rules: {
-    // Override default rules
-    'arrow-parens': ['error', 'as-needed', {'requireForBlockBody': true}],
+    // Disable Prettier
+    'prettier/prettier': 'off',
+
+    // StraboSpot2 Override default rules
+    'arrow-parens': ['error', 'as-needed', {requireForBlockBody: true}],
     'brace-style': ['error', 'stroustrup', {allowSingleLine: true}], // Prettier can't do this so don't use prettier
     'curly': ['error', 'multi-line'],
     'jsx-quotes': ['error', 'prefer-single'],
     'no-unused-vars': ['error', {args: 'none', ignoreRestSiblings: true, destructuredArrayIgnorePattern: '[a-z]'}],
-    'object-curly-newline': ['error', {'consistent': true}],
+    'object-curly-newline': ['error', {consistent: true}],
     'operator-linebreak': ['error', 'before'],
     'quote-props': ['error', 'consistent'],
     'quotes': ['error', 'single'],
 
-    // Override Import rules
+    // StraboSpot2 Override Import rules
     'import/order': ['error', {
       'groups': ['builtin', 'external', 'internal'],
       'pathGroups': [{pattern: '{react,react-native}', group: 'external', position: 'before'}],
@@ -35,18 +37,8 @@ module.exports = {
       'alphabetize': {order: 'asc', caseInsensitive: true},
     }],
 
-    // Override React Native rules
-    'react-native/no-inline-styles': 'off',
-
-    // Override React rules
+    // StraboSpot2 Override React rules
     'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx']}], // allow .js files to contain JSX code
-
-    // Override React Hooks rules
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
-
-    // Turn off rules
-    'react/prop-types': 'off',
   },
   settings: {
     'import/ignore': ['react-native'],
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // Prettier is disabled in .eslintrc.js
+
+  // Default Rules
+  // arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+
+  // StraboSpot2 Rules
+  quoteProps: 'consistent',
+}
