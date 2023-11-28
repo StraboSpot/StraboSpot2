867df63b5069c8396f9be0c5664e7d3eff934dd3
Nathan Novak <nnovak@ku.edu>
2023-11-28 10:20:40 -0700

    refactor(App.js): Renamed App.tsx to App.js
    We are not using typescript

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ const babelLoaderConfiguration = {
   // Add every directory that needs to be compiled by Babel during the build.
   include: [
     path.resolve(__dirname, 'index.web.js'), // Entry to your application
-    path.resolve(__dirname, 'App.tsx'), // Change this to your main App file
+    path.resolve(__dirname, 'App.js'), // Change this to your main App file
     path.resolve(__dirname, 'src'),
     ...compileNodeModules,
   ],
