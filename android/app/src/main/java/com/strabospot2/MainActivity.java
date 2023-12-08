8cb5a2c48fa3cbdd156f1697f2a96c9906f79c95
Nathan Novak <nnovak@ku.edu>
2023-12-08 09:11:49 -0700

    build(package.json): Added react-native-orientation-locker to project

--- android/app/src/main/java/com/strabospot2/MainActivity.java
+++ android/app/src/main/java/com/strabospot2/MainActivity.java
@@ -7,9 +7,18 @@ import com.facebook.react.defaults.DefaultReactActivityDelegate;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowInsets;
+import android.content.Intent;
+import android.content.res.Configuration;
 
 public class MainActivity extends ReactActivity {
 
+ @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    Intent intent = new Intent("onConfigurationChanged");
+    intent.putExtra("newConfig", newConfig);
+    this.sendBroadcast(intent);
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
