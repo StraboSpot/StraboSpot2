3c5e89d650b6a3bfb2a0293ef9e42b6d94439244
Jessica Good <jessicagood012@gmail.com>
2024-02-22 11:19:34 -0700

    refactor(main menu): Simplified

--- src/modules/main-menu-panel/MainMenuPanel.js
+++ src/modules/main-menu-panel/MainMenuPanel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView} from 'react-native';
 
 import {useDispatch, useSelector} from 'react-redux';
 
@@ -7,7 +7,7 @@ import About from './About';
 import Documentation from './Documentation';
 import {MAIN_MENU_ITEMS} from './mainMenu.constants';
 import {setMenuSelectionPage} from './mainMenuPanel.slice';
-import styles from './mainMenuPanel.styles';
+import mainMenuPanelStyles from './mainMenuPanel.styles';
 import MainMenuPanelHeader from './MainMenuPanelHeader';
 import MainMenuPanelList from './MainMenuPanelList';
 import {isEmpty} from '../../shared/Helpers';
@@ -40,169 +40,75 @@ const MainMenuPanel = ({
   const project = useSelector(state => state.project.project);
   const settingsPageVisible = useSelector(state => state.mainMenu.mainMenuPageVisible);
 
-  const mainMenuHeader = (
-    <MainMenuPanelHeader
-      onPress={() => dispatch(setMenuSelectionPage({name: null}))}>
-      {settingsPageVisible}
-    </MainMenuPanelHeader>
-  );
-  let page;
+  const getMainMenuContent = () => {
+    switch (settingsPageVisible) {
+      case MAIN_MENU_ITEMS.MANAGE.MY_STRABOSPOT:
+        return <MyStraboSpot logout={logout}/>;
+      case MAIN_MENU_ITEMS.MANAGE.ACTIVE_PROJECTS:
+        return (
+          <ActiveProject
+            title={!isEmpty(project) && project.description ? project.description.project_name : 'Un-named'}
+          />
+        );
+      case MAIN_MENU_ITEMS.MANAGE.UPLOAD_BACKUP_EXPORT:
+        return <UploadBackupAndExport closeMainMenuPanel={closeMainMenuPanel}/>;
+      case MAIN_MENU_ITEMS.ATTRIBUTES.SPOTS_LIST:
+        return <SpotsList onPress={openSpotInNotebook} updateSpotsInMapExtent={updateSpotsInMapExtent}/>;
+      case MAIN_MENU_ITEMS.ATTRIBUTES.IMAGE_GALLERY:
+        return <ImageGallery openSpotInNotebook={openSpotInNotebook} updateSpotsInMapExtent={updateSpotsInMapExtent}/>;
+      case MAIN_MENU_ITEMS.ATTRIBUTES.SAMPLES:
+        return (
+          <SamplesMenuItem
+            openSpotInNotebook={openSpotInNotebook}
+            updateSpotsInMapExtent={updateSpotsInMapExtent}
+          />
+        );
+      case MAIN_MENU_ITEMS.ATTRIBUTES.GEOLOGIC_UNITS:
+        return <Tags type={'geologic_unit'}/>;
+      case MAIN_MENU_ITEMS.ATTRIBUTES.TAGS:
+        return <Tags/>;
+      case MAIN_MENU_ITEMS.MAPS.CUSTOM:
+        return <CustomMapsMenu zoomToCustomMap={zoomToCustomMap}/>;
+      case MAIN_MENU_ITEMS.MAPS.IMAGE_BASEMAPS :
+        return <ImageBasemapsList closeManMenuPanel={closeMainMenuPanel}/>;
+      case MAIN_MENU_ITEMS.MAPS.STRAT_SECTIONS :
+        return <StratSectionsList closeManMenuPanel={closeMainMenuPanel}/>;
+      case MAIN_MENU_ITEMS.MAPS.MANAGE_OFFLINE_MAPS:
+        return (
+          <ManageOfflineMapsMenu
+            closeMainMenuPanel={closeMainMenuPanel}
+            zoomToCenterOfflineTile={zoomToCenterOfflineTile}
+          />
+        );
+      case MAIN_MENU_ITEMS.PREFERENCES.SHORTCUTS:
+        return <ShortcutMenu/>;
+      case MAIN_MENU_ITEMS.PREFERENCES.NAMING_CONVENTIONS:
+        return <NamingConventions/>;
+      case MAIN_MENU_ITEMS.PREFERENCES.MISCELLANEOUS:
+        return <Miscellaneous/>;
+      case MAIN_MENU_ITEMS.HELP.ABOUT:
+        return <About/>;
+      case MAIN_MENU_ITEMS.HELP.DOCUMENTATION:
+        return <Documentation/>;
+      default:
+        return (
+          <MainMenuPanelList
+            onPress={setVisibleMenu}
+            activeProject={!isEmpty(project) && project.description ? project.description.project_name
+              : 'No Active Project'}
+          />
+        );
+    }
+  };
 
   const setVisibleMenu = (name) => {
     dispatch(setMenuSelectionPage({name: name}));
   };
 
-  switch (settingsPageVisible) {
-    case MAIN_MENU_ITEMS.MANAGE.MY_STRABOSPOT:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <MyStraboSpot logout={logout}/>
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.MANAGE.ACTIVE_PROJECTS:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <ActiveProject
-            title={!isEmpty(project) && project.description ? project.description.project_name : 'Un-named'}
-          />
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.MANAGE.UPLOAD_BACKUP_EXPORT:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <UploadBackupAndExport closeMainMenuPanel={closeMainMenuPanel}/>
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.ATTRIBUTES.SPOTS_LIST:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <SpotsList
-            onPress={openSpotInNotebook}
-            updateSpotsInMapExtent={updateSpotsInMapExtent}
-          />
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.ATTRIBUTES.IMAGE_GALLERY:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <ImageGallery
-            openSpotInNotebook={openSpotInNotebook}
-            updateSpotsInMapExtent={updateSpotsInMapExtent}
-          />
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.ATTRIBUTES.SAMPLES:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <SamplesMenuItem
-            openSpotInNotebook={openSpotInNotebook}
-            updateSpotsInMapExtent={updateSpotsInMapExtent}
-          />
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.ATTRIBUTES.GEOLOGIC_UNITS:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <Tags type={'geologic_unit'}/>
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.ATTRIBUTES.TAGS:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <Tags/>
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.MAPS.CUSTOM:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <CustomMapsMenu
-            zoomToCustomMap={zoomToCustomMap}
-          />
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.MAPS.IMAGE_BASEMAPS :
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <ImageBasemapsList closeManMenuPanel={closeMainMenuPanel}/>
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.MAPS.STRAT_SECTIONS :
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <StratSectionsList closeManMenuPanel={closeMainMenuPanel}/>
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.MAPS.MANAGE_OFFLINE_MAPS:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <ManageOfflineMapsMenu
-            closeMainMenuPanel={closeMainMenuPanel}
-            zoomToCenterOfflineTile={zoomToCenterOfflineTile}/>
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.PREFERENCES.SHORTCUTS:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <ShortcutMenu/>
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.PREFERENCES.NAMING_CONVENTIONS:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <NamingConventions/>
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.PREFERENCES.MISCELLANEOUS:
-      page = (
-        <Miscellaneous/>
-      );
-      break;
-    case MAIN_MENU_ITEMS.HELP.ABOUT:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <About/>
-        </View>
-      );
-      break;
-    case MAIN_MENU_ITEMS.HELP.DOCUMENTATION:
-      page = (
-        <View style={styles.mainMenuContainer}>
-          <Documentation/>
-        </View>
-      );
-      break;
-    default:
-      page = (
-        <React.Fragment>
-          <MainMenuPanelList
-            onPress={name => setVisibleMenu(name)}
-            activeProject={!isEmpty(project) && project.description ? project.description.project_name
-              : 'No Active Project'}
-          />
-        </React.Fragment>
-      );
-  }
-
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={styles.container}>
-        {mainMenuHeader}
-        {page}
-      </View>
+    <SafeAreaView style={mainMenuPanelStyles.container}>
+      <MainMenuPanelHeader onPress={() => dispatch(setMenuSelectionPage({name: null}))}/>
+      {getMainMenuContent()}
     </SafeAreaView>
   );
 };
