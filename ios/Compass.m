18e40e9fda3f3683f7b4bb6fcff7d0d08526fb2d
Nathan Novak <nnovak@ku.edu>
2024-02-22 11:47:08 -0700

    build(iOS Compass): Moving the math logic to the JS side so as not to have duplicate code for both platforms.
    Also removed unused external method. Minor changes to Podfile and Podfile.lock.

--- ios/Compass.m
+++ ios/Compass.m
@@ -11,9 +11,7 @@
 #import "React/RCTEventEmitter.h"
 
 @interface RCT_EXTERN_MODULE(Compass, RCTEventEmitter)
-//RCT_EXTERN_METHOD(getHeading)
-RCT_EXTERN_METHOD(myAccelerometer)
-RCT_EXTERN_METHOD(myAcceleration)
+RCT_EXTERN_METHOD(getHeading)
 RCT_EXTERN_METHOD(myDeviceRotation)
 RCT_EXTERN_METHOD(stopObserving)
 
--- ios/Compass.swift
+++ ios/Compass.swift
@@ -17,32 +17,29 @@ class Compass: RCTEventEmitter {
   var motion = CMMotionManager()
   let locationManager = CLLocationManager()
   private var count = 0;
-  private var accelerationX: Double = 0;
-  private var accelerationY:  Double = 0;
-  private var accelerationZ: Double = 0;
   private var heading: Double = 0
 
-  @objc
-  func myAccelerometer() {
-    if motion.isAccelerometerAvailable {
-      print("available")
-      motion.accelerometerUpdateInterval = 0.1
-      motion.startAccelerometerUpdates(to: OperationQueue.main) { (data, error) in
-            if let trueData = data {
-              self.accelerationX = Double(trueData.acceleration.x).rounded(toPlaces: 3)
-              self.accelerationY = Double(trueData.acceleration.y).rounded(toPlaces: 3)
-              self.accelerationZ = Double(trueData.acceleration.z).rounded(toPlaces: 3)
-              self.sendEvent(withName: "acceleration",
-                             body: [
-                              "accelerationX": self.accelerationX,
-                              "accelerationY": self.accelerationY,
-                              "accelerationZ": self.accelerationZ
-                            ])
-                    }
-                }
-
-    } else {print("not available")}
-  }
+//  @objc
+//  func myAccelerometer() {
+//    if motion.isAccelerometerAvailable {
+//      print("available")
+//      motion.accelerometerUpdateInterval = 0.1
+//      motion.startAccelerometerUpdates(to: OperationQueue.main) { (data, error) in
+//            if let trueData = data {
+//              self.accelerationX = Double(trueData.acceleration.x).rounded(toPlaces: 3)
+//              self.accelerationY = Double(trueData.acceleration.y).rounded(toPlaces: 3)
+//              self.accelerationZ = Double(trueData.acceleration.z).rounded(toPlaces: 3)
+//              self.sendEvent(withName: "acceleration",
+//                             body: [
+//                              "accelerationX": self.accelerationX,
+//                              "accelerationY": self.accelerationY,
+//                              "accelerationZ": self.accelerationZ
+//                            ])
+//                    }
+//                }
+//
+//    } else {print("not available")}
+//  }
 
   @objc
   func myDeviceRotation() {
@@ -63,10 +60,18 @@ class Compass: RCTEventEmitter {
                   }
                   let rotationMatrix = data.attitude.rotationMatrix
                   self.matrixArray.append(rotationMatrix)
+            print("MatrixArray", self.matrixArray);
+            print("MatrixArray", self.matrixArray.count);
+
                   if (self.matrixArray.count > 5){
+
                     self.matrixArray.removeFirst();
+                    print("MatrixArray", self.matrixArray);
                   }
 
+            let m11Avg = self.matrixArray.map{$0.m11}.reduce(0,+) / Double(self.matrixArray.count)
+            let m12Avg = self.matrixArray.map{$0.m12}.reduce(0,+) / Double(self.matrixArray.count)
+            let m13Avg = self.matrixArray.map{$0.m13}.reduce(0,+) / Double(self.matrixArray.count)
             let m21Avg = self.matrixArray.map{$0.m21}.reduce(0,+) / Double(self.matrixArray.count)
             let m22Avg = self.matrixArray.map{$0.m22}.reduce(0,+) / Double(self.matrixArray.count)
             let m23Avg = self.matrixArray.map{$0.m23}.reduce(0,+) / Double(self.matrixArray.count)
@@ -75,94 +80,97 @@ class Compass: RCTEventEmitter {
             let m33Avg = self.matrixArray.map{$0.m33}.reduce(0,+) / Double(self.matrixArray.count)
 
 
-                  let ENU_pole = self.cartesianToSpherical(mValue1: -m32Avg, mValue2: m31Avg,mValue3: m33Avg)
-                  let ENU_TP = self.cartesianToSpherical(mValue1: -m22Avg, mValue2: m21Avg, mValue3: m23Avg)
-
+//                  let ENU_pole = self.cartesianToSpherical(mValue1: -m32Avg, mValue2: m31Avg,mValue3: m33Avg)
+//                  let ENU_TP = self.cartesianToSpherical(mValue1: -m22Avg, mValue2: m21Avg, mValue3: m23Avg)
+//
                   let heading = self.mod(value: (data.heading - 270), degrees: 360).rounded(toPlaces: 0)
-                  let strikeAndDipData = self.strikeAndDip(array: ENU_pole)
-                  let trendAndPlungeData = self.trendAndPlunge(array: ENU_TP)
-                  let strike = Double(strikeAndDipData[0]).rounded(toPlaces: 0);
-                  let dip = Int(strikeAndDipData[1]);
-                  let trend = Double(trendAndPlungeData[0]).rounded(toPlaces: 0);
-                  let plunge = Int(trendAndPlungeData[1]);
+//                  let strikeAndDipData = self.strikeAndDip(array: ENU_pole)
+//                  let trendAndPlungeData = self.trendAndPlunge(array: ENU_TP)
+//                  let strike = Double(strikeAndDipData[0]).rounded(toPlaces: 0);
+//                  let dip = Int(strikeAndDipData[1]);
+//                  let trend = Double(trendAndPlungeData[0]).rounded(toPlaces: 0);
+//                  let plunge = Int(trendAndPlungeData[1]);
+
+//                  print(rotationMatrix)
 
                 self.sendEvent(withName: "rotationMatrix",
                            body: [
-                            "strike": strike,
-                            "dip": dip,
-                            "trend": trend,
-                            "plunge": plunge,
+//                            "strike": strike,
+//                            "dip": dip,
+//                            "trend": trend,
+//                            "plunge": plunge,
                             "heading": heading,
-                            "M11": rotationMatrix.m11,
-                            "M12": rotationMatrix.m12,
-                            "M13": rotationMatrix.m13,
-                            "M21": rotationMatrix.m21,
-                            "M22": rotationMatrix.m22,
-                            "M23": rotationMatrix.m23,
-                            "M31": rotationMatrix.m31,
-                            "M32": rotationMatrix.m32,
-                            "M33": rotationMatrix.m33,
+
+                            "M11": m11Avg,
+                            "M12": m12Avg,
+                            "M13": m13Avg,
+                            "M21": m21Avg,
+                            "M22": m22Avg,
+                            "M23": m23Avg,
+                            "M31": m31Avg,
+                            "M32": m32Avg,
+                            "M33": m33Avg,
                             ])
           })
       }
 
-      func cartesianToSpherical (mValue1: Double, mValue2: Double, mValue3: Double) -> [Double]{
-          var rho = (pow(mValue1, 2) + pow(mValue2, 2) + pow(mValue3, 2)).squareRoot();
-          var phi: Double = 0;
-          var theta: Double = 0;
-          if (rho == 0) {
-              phi = 0;
-              theta = 0;
-          }
-          else{
-              phi = Double((acos(mValue3/rho)))
-              if (rho * sin((phi)) == 0) {
-                  if (mValue3 >= 0){
-                      rho = mValue3;
-                      phi = 0;
-                      theta = 0;
-                  }
-                  else {
-                      rho = -mValue3;
-                      phi = Double.pi
-                      theta = 0;
-                  }
-              }
-              else {
-                  theta = atan2(mValue2, mValue1)
-              }
-          }
-          return [rho, phi, theta]
-      }
+//      func cartesianToSpherical (mValue1: Double, mValue2: Double, mValue3: Double) -> [Double]{
+//          var rho = (pow(mValue1, 2) + pow(mValue2, 2) + pow(mValue3, 2)).squareRoot();
+//          var phi: Double = 0;
+//          var theta: Double = 0;
+//          if (rho == 0) {
+//              phi = 0;
+//              theta = 0;
+//          }
+//          else{
+//              phi = Double((acos(mValue3/rho)))
+//              if (rho * sin((phi)) == 0) {
+//                  if (mValue3 >= 0){
+//                      rho = mValue3;
+//                      phi = 0;
+//                      theta = 0;
+//                  }
+//                  else {
+//                      rho = -mValue3;
+//                      phi = Double.pi
+//                      theta = 0;
+//                  }
+//              }
+//              else {
+//                  theta = atan2(mValue2, mValue1)
+//              }
+//          }
+//          return [rho, phi, theta]
+//      }
 
-      func strikeAndDip (array: Array<Double> ) -> [Double] {
-          let phi = array[1]
-          let theta = array[2]
-          var strikeDeg: Double = 0;
-          var dipDeg: Double = 0;
-          if (phi <= Double.pi/2) {
-              strikeDeg = Double(mod(value: (Double(360 - theta * (180/Double.pi))), degrees: 360));
-              dipDeg = phi * (180/Double.pi)
+//      func strikeAndDip (array: Array<Double> ) -> [Double] {
+//          let phi = array[1]
+//          let theta = array[2]
+//          var strikeDeg: Double = 0;
+//          var dipDeg: Double = 0;
+//          if (phi <= Double.pi/2) {
+//              strikeDeg = Double(mod(value: (Double(360 - theta * (180/Double.pi))), degrees: 360));
+//              dipDeg = phi * (180/Double.pi)
+//
+//          }
+//          else {
+//              strikeDeg = Double(mod(value: 360 - (theta + Double.pi) * (180/Double.pi), degrees: 360));
+//              dipDeg = (Double.pi - phi) * (180/Double.pi)
+//          }
+//          return [strikeDeg, dipDeg]
+//      }
 
-          }
-          else {
-              strikeDeg = Double(mod(value: 360 - (theta + Double.pi) * (180/Double.pi), degrees: 360));
-              dipDeg = (Double.pi - phi) * (180/Double.pi)
-          }
-          return [strikeDeg, dipDeg]
-      }
-
-      func trendAndPlunge (array: Array<Double>) -> [Double] {
-          let phi = array[1];
-          let theta = array[2];
-          var trendDeg: Double = Double(mod(value: 90 - theta * (180/Double.pi), degrees: 360));
-          var plungeDeg: Double = phi * (180/Double.pi) - 90;
-          if (plungeDeg < 0) {
-              trendDeg = Double(mod(value: trendDeg + 180, degrees: 360));
-              plungeDeg = -plungeDeg;
-          }
-          return [trendDeg, plungeDeg]
-      }
+//      func trendAndPlunge (array: Array<Double>) -> [Double] {
+//          let phi = array[1];
+//          let theta = array[2];
+//          var trendDeg: Double = Double(mod(value: 90 - theta * (180/Double.pi), degrees: 360));
+//          var plungeDeg: Double = phi * (180/Double.pi) - 90;
+//          if (plungeDeg < 0) {
+//              trendDeg = Double(mod(value: trendDeg + 180, degrees: 360));
+//              plungeDeg = -plungeDeg;
+//          }
+//          return [trendDeg, plungeDeg]
+//      }
 
       func mod(value: Double, degrees: Double) -> Double {
           return ((value .truncatingRemainder(dividingBy: degrees) ) + degrees) .truncatingRemainder(dividingBy: (degrees))
@@ -173,13 +181,15 @@ class Compass: RCTEventEmitter {
   override func stopObserving() {
     motion.stopAccelerometerUpdates()
     motion.stopDeviceMotionUpdates()
+//    motion.stopAccelerometerUpdates()
+//    motion.stopDeviceMotionUpdates()
     locationManager.stopUpdatingHeading()
     print("Compass Events Stopped in iOS code")
   }
 
   // we need to override this method and
    // return an array of event names that we can listen to
-   override func supportedEvents() -> [String]! {
+  override func supportedEvents() -> [String]! {
      return ["rotationMatrix"]
    }
 
@@ -189,16 +199,14 @@ class Compass: RCTEventEmitter {
   }
 }
 
-extension Double {
-  func mod(value: Double, degrees: Double) -> Double {
-      return ((value .truncatingRemainder(dividingBy: degrees) ) + degrees) .truncatingRemainder(dividingBy: (degrees))
-  }
-}
+//extension Double {
+//  func mod(value: Double, degrees: Double) -> Double {
+//      return ((value .truncatingRemainder(dividingBy: degrees) ) + degrees) .truncatingRemainder(dividingBy: (degrees))
+//  }
+//}
 extension Double {
     func rounded(toPlaces places: Int) -> Double {
         let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded()/divisor
     }
 }
-
-
