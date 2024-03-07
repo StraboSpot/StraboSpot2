60a973bd8a71112b26b2c50a5e66b3002ca5c516
Nathan Novak <nnovak@ku.edu>
2024-03-07 07:25:30 -0700

    build(package.json): Rolled back RN to 0.72.4 so the Chrome debugger will work with iOS.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
   env: {
     production: {
       plugins: ['transform-remove-console'],
