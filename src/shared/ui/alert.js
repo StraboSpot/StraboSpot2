b3f289fab3185a9fa72cd5bbfa89f9a3aeb2e509
Jessica Good <jessicagood012@gmail.com>
2024-05-22 12:45:34 -0700

    fix(web): Fixed alert messages error

--- src/shared/ui/alert.js
+++ src/shared/ui/alert.js
@@ -3,12 +3,12 @@ import {Alert, Platform} from 'react-native';
 const alertPolyfill = (title, description, options, extra) => {
   const result = window.confirm([title, description].filter(Boolean).join('\n'));
 
-  if (result) {
+  if (result && options) {
     console.log('OK pressed');
     const confirmOption = options.find(({style}) => style !== 'cancel');
     confirmOption && confirmOption.onPress();
   }
-  else {
+  else if (options) {
     console.log('Cancel pressed');
     const cancelOption = options.find(({style}) => style === 'cancel');
     cancelOption && cancelOption.onPress();
@@ -17,7 +17,7 @@ const alertPolyfill = (title, description, options, extra) => {
 
 const alert = (title, description, options, extra) => {
   Platform.OS === 'web' ? alertPolyfill(title, description, options, extra)
-    : Alert.alert(title,  description,options || [{ text: 'Ok', onPress: () => console.log('OK Pressed') }]);
-}
+    : Alert.alert(title, description, options || [{text: 'Ok', onPress: () => console.log('OK Pressed')}]);
+};
 
 export default alert;
