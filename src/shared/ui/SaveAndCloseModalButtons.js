d225d93ff8966530b836f48cecd5bbfb0cb9d086
Jessica Good <jessicagood012@gmail.com>
2024-06-07 15:04:26 -0700

    refactor(style): Use accent color for Save buttons

--- src/shared/ui/SaveAndCloseModalButtons.js
+++ src/shared/ui/SaveAndCloseModalButtons.js
@@ -4,7 +4,7 @@ import {ButtonGroup} from 'react-native-elements';
 import {useDispatch} from 'react-redux';
 
 import {setModalVisible} from '../../modules/home/home.slice';
-import {POSITIVE_COLOR} from '../styles.constants';
+import {PRIMARY_ACCENT_COLOR} from '../styles.constants';
 
 const SaveAndCloseModalButtons = ({
                                     saveAction,
@@ -22,15 +22,11 @@ const SaveAndCloseModalButtons = ({
   return (
     <ButtonGroup
       buttons={['Save', 'Save and Close']}
-      selectedIndex={selectedIndex}
-      onPress={onPress}
-      textStyle={{color: POSITIVE_COLOR}}
       containerStyle={{height: 40, borderRadius: 10, marginTop: 10, marginBottom: 10}}
-      buttonStyle={{padding: 5}}
-      selectedButtonStyle={{backgroundColor: POSITIVE_COLOR}}
-      disabledSelectedStyle={{type: 'outline', borderColor: POSITIVE_COLOR, backgroundColor: 'pink'}}
-      disabledSelectedTextStyle={{type: 'outline', borderColor: POSITIVE_COLOR, backgroundColor: 'yellow'}}
-      disabledStyle={{type: 'outline', borderColor: POSITIVE_COLOR, backgroundColor: 'blue'}}
+      onPress={onPress}
+      selectedButtonStyle={{backgroundColor: PRIMARY_ACCENT_COLOR}}
+      selectedIndex={selectedIndex}
+      textStyle={{color: PRIMARY_ACCENT_COLOR}}
     />
   );
 };
