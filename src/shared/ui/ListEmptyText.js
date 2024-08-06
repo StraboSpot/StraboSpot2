168d52c28a203486f4c2bca0c527e1608de97591
Nathan Novak <nnovak@ku.edu>
2024-08-06 06:27:02 -0700

    fix(Lists): Minor formatting to the ListEmptyText component

--- src/shared/ui/ListEmptyText.js
+++ src/shared/ui/ListEmptyText.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 
 import commonStyles from '../common.styles';
 
-const ListEmptyText = ({text, textStyle}) => {
-  return <Text style={[commonStyles.noValueText, textStyle]}>{text}</Text>;
+const ListEmptyText = ({text, textStyle, containerStyle}) => {
+  return (
+    <View style={containerStyle}>
+      <Text style={[commonStyles.noValueText, textStyle]}>{text}</Text>
+    </View>
+)
 };
 
 export default ListEmptyText;
