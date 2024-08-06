a9f0a20afade9ffd66f3bbd4f2c2ba6b6be39544
Nathan Novak <nnovak@ku.edu>
2024-08-06 06:25:30 -0700

    fix(Offline Maps): Additional commit to adding the refresh button

--- src/shared/ui/SectionDividerWithRightButton.js
+++ src/shared/ui/SectionDividerWithRightButton.js
@@ -8,13 +8,21 @@ import styles from '../../shared/ui/ui.styles';
 import commonStyles from '../common.styles';
 import {PRIMARY_ACCENT_COLOR} from '../styles.constants';
 
-const SectionDividerWithRightButton = ({buttonTitle, disabled, dividerText, onPress}) => {
+const SectionDividerWithRightButton = ({buttonTitle, disabled, dividerText, iconName, iconType, iconSize, onPress}) => {
   return (
     <View style={styles.sectionDividerWithButtonContainer}>
       <SectionDivider dividerText={dividerText}/>
       <Button
         title={buttonTitle}
-        icon={!buttonTitle && <Icon name={'add'} style={{paddingRight: 10, paddingLeft: 10}} size={30} color={PRIMARY_ACCENT_COLOR}/>}
+        icon={!buttonTitle
+          && (
+            <Icon name={iconName || 'add'}
+                  style={{paddingRight: 10, paddingLeft: 10}}
+                  size={ iconSize || 30}
+                  color={PRIMARY_ACCENT_COLOR}
+                  type={iconType}
+            />)
+        }
         titleStyle={commonStyles.standardButtonText}
         type={'clear'}
         onPress={onPress}
