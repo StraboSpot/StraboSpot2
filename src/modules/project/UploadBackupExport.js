0c911ae14aaba088346b81adb9abbf5ba2b92358
Nathan Novak <nnovak@ku.edu>
2024-08-27 12:49:29 -0700

    feat(Backup and Upload Modals): Made the modals a local state

--- src/modules/project/UploadBackupExport.js
+++ src/modules/project/UploadBackupExport.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Linking, Platform, Text, View} from 'react-native';
 
 import {Button} from 'react-native-elements';
@@ -16,9 +16,9 @@ import {
   addedStatusMessage,
   clearedStatusMessages,
   setIsErrorMessagesModalVisible,
-  setIsUploadModalVisible,
 } from '../home/home.slice';
 import overlayStyles from '../home/overlays/overlay.styles';
+import {BackupModal, UploadModal} from '../project/modals/index';
 
 const UploadBackAndExport = () => {
   const dispatch = useDispatch();
@@ -33,7 +33,7 @@ const UploadBackAndExport = () => {
   const checkForActiveDatasets = () => {
     if (activeDatasets.length > 0) {
       dispatch(setSelectedProject({source: '', project: ''}));
-      dispatch(setIsBackupModalVisible(true));
+      setIsBackupModalVisible(true);
     }
     else {
       dispatch(clearedStatusMessages());
@@ -57,7 +57,7 @@ const UploadBackAndExport = () => {
             titleStyle={commonStyles.standardButtonText}
             onPress={() => {
               dispatch(setSelectedProject({source: '', project: ''}));
-              dispatch(setIsUploadModalVisible(true));
+              setIsUploadModalVisible(true);
             }}
             disabled={!isOnline.isConnected}
           />
@@ -123,6 +123,14 @@ const UploadBackAndExport = () => {
           </View>
         )
       }
+      <BackupModal
+        visible={isBackupModalVisible}
+        closeModal={() => setIsBackupModalVisible(false)}
+      />
+      <UploadModal
+        visible={isUploadModalVisible}
+        closeModal={() => setIsUploadModalVisible(false)}
+      />
       {/*<Divider sectionText={'export'}/>*/}
       {/*{renderExportButtons()}*/}
       {/*<Divider sectionText={'restore project from backup'}/>*/}
