526aaeb0c68ded9b3540cb9b296869c020c44055
Nathan Novak <nnovak@ku.edu>
2024-08-27 12:33:49 -0700

    refactor(Moved the backup and upload modals into the UploadBackupExport.js module and changed to use local state to display.  Taking these out of home makes them render less.):

---
+++ src/modules/project/modals/index.js
@@ -0,0 +1,3 @@
+export {default as BackupModal} from '../../project/modals/BackupModal';
+export {default as UploadModal} from '../../project/modals/UploadModal';
+export {default as UploadProgressModal} from './UploadProgressModal';
