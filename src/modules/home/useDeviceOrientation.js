eee75c0979e738bf064c7c832d44e42daabe2ed7
Jessica Good <jessicagood012@gmail.com>
2024-10-24 16:24:04 -0700

    refactor(Device): Get orientation when needed to lock instead of saving in state

--- src/modules/home/useDeviceOrientation.js
+++ src/modules/home/useDeviceOrientation.js
@@ -1,29 +1,29 @@
-import {useState} from 'react';
-
-import Orientation, {PORTRAIT, PORTRAIT_UPSIDE_DOWN, useDeviceOrientationChange} from 'react-native-orientation-locker';
+import Orientation, {
+  LANDSCAPE_LEFT,
+  LANDSCAPE_RIGHT,
+  PORTRAIT,
+  PORTRAIT_UPSIDE_DOWN,
+} from 'react-native-orientation-locker';
 import {useToast} from 'react-native-toast-notifications';
 
 const useDeviceOrientation = () => {
-  const [orientation, setOrientation] = useState(null);
 
   const toast = useToast();
 
   const lockOrientation = () => {
-    // console.log('Orientation', orientation);
-    if (orientation === PORTRAIT || orientation === PORTRAIT_UPSIDE_DOWN) Orientation.lockToPortrait();
-    else Orientation.lockToLandscape();
-    toast.show('Screen orientation LOCKED in EDIT mode');
+    console.log('Locking device orientation...');
+    Orientation.getDeviceOrientation((deviceOrientation) => {
+      console.log('Current Device Orientation: ', deviceOrientation);
+      if (deviceOrientation === PORTRAIT || deviceOrientation === PORTRAIT_UPSIDE_DOWN) Orientation.lockToPortrait();
+      else if (deviceOrientation === LANDSCAPE_LEFT || deviceOrientation === LANDSCAPE_RIGHT) Orientation.lockToLandscape();
+      toast.show('Screen orientation LOCKED in EDIT mode');
+    });
   };
 
   const unlockOrientation = () => {
     Orientation.unlockAllOrientations();
     toast.show('Screen orientation UNLOCKED');
   };
-
-  useDeviceOrientationChange((o) => {
-    // console.log('Orientation Change', o);
-    setOrientation(o);
-  });
 
   return {
     lockOrientation: lockOrientation,
