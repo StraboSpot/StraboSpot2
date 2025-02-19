ebb6ce272ccab7764f0bb1e9392a83f3d6c7d60d
Nathan Novak <nnovak@ku.edu>
2025-02-19 08:48:44 -0700

    feat(IGSN): Added the url constants and extra keys to the sesar JSON. Needs cleanup

--- src/modules/samples/useSamples.js
+++ src/modules/samples/useSamples.js
@@ -1,18 +1,49 @@
+import {useState} from 'react';
+import {Linking} from 'react-native';
+
+import {useDispatch} from 'react-redux';
+
+import {isEmpty, unixToDateTime} from '../../shared/Helpers';
+import config from '../../utils/config';
 import {useSpots} from '../spots';
+import {setSesarToken} from '../user/userProfile.slice';
+import {ORCID_PATHS, SESAR_PATHS} from '../../services/urls.constants';
 // import convert from 'xml-js';
 
-const useSamples = () => {
+const useSamples = (selectedFeature) => {
+const {SESAR_API, GET_TOKEN, GET_USER_CODE} = SESAR_PATHS;
+const {ORCID, AUTH, SCOPE, REDIRECT_URL} = ORCID_PATHS;
+  const dispatch = useDispatch();
+
   const {getAllSpotSamplesCount} = useSpots();
 
-  const selectedSampleData = (selectedFeature) => {
+  const [isOrcidSignInPrompt, setIsOrcidSignInPrompt] = useState(true);
+
+  const getRockClassification = (materialType) => {
+    if (materialType === 'intact_rock' || materialType === 'fragmented_rock') {
+
+    }
+  }
+
+  const selectedSampleData = () => {
     // console.log('Selected Feature in useSamples', selectedFeature);
-    const {collection_date, collection_time, latitude, longitude,material_type, sample_type, sample_id_name} = selectedFeature;
+    const {
+      collection_date,
+      collection_time,
+      latitude,
+      longitude,
+      material_type,
+      rock_classification,
+      sample_type,
+      sample_id_name,
+    } = selectedFeature;
     const selectedFeatureJSON = {
       collection_date: collection_date,
       collection_time: collection_time,
       latitude: latitude,
       longitude: longitude,
       material_type: material_type,
+      rock_classification: getRockClassification(material_type),
       sample_id_name: sample_id_name,
       sample_type: sample_type,
     };
@@ -25,18 +56,84 @@ const useSamples = () => {
     return count;
   };
 
+  const getOrcidToken = async (encoded_login) => {
+    try {
+      // if (isEmpty(orcidToken)) {
+      const url = ORCID + AUTH + SCOPE + REDIRECT_URL + encodeURIComponent(encoded_login);
+      // const url = `https://orcid.org/oauth/authorize
+      // ?client_id=${ORCID_CLIENT_ID}&response_type=code&scope=openid
+      // &redirect_uri=https://www.strabospot.org/orcid_callback%3Fcreds%3D${encodeURIComponent(encoded_login)}`;
+      console.log(url);
+      // await Linking.openURL(url);
+      // }
+      // else {
+      //   console.log('Check user ORCID token');
+      // }
+    }
+    catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getSesarToken = async (token) => {
+    const formData = new FormData();
+    formData.append('connection', 'strabospot');
+    formData.append('orcid_id_token', token);
+
+    try {
+      const sesarToken = await fetch(SESAR_API + GET_TOKEN, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Accept': 'application/json',
+        },
+        // body: JSON.stringify({connection: 'strabospot', orcid_id_token: orcidToken}),
+        body: formData,
+      });
+      const sesarJson = await sesarToken.json();
+      if (sesarJson.error) {
+        console.error('SESAR Token Error', sesarJson.error);
+        setIsOrcidSignInPrompt(true);
+      }
+      else {
+        console.log(sesarJson);
+        const accessTokenParsed = JSON.parse(atob(sesarJson.access.split('.')[1]));
+        const expDateTime = unixToDateTime(accessTokenParsed.exp).toUTCString();
+        !isEmpty(sesarJson) && dispatch(setSesarToken({access: sesarJson.access, refresh: sesarJson.refresh, expiration: expDateTime}));
+        await getSesarUserCode(sesarJson);
+        // console.log('USER CODE', userCode);
+        setIsOrcidSignInPrompt(false);
+      }
+    }
+    catch (err) {
+      console.log('SESAR Token Error:', err);
+    }
+  };
+
   const getSesarUserCode = async (token) => {
     try {
-      const url = 'https://app.geosamples.org/webservices/credentials_service_v2.php';
-      console.log(JSON.parse(atob(token.access.split('.')[1])));
-      fetch(url, {
+      const myHeaders = new Headers();
+      myHeaders.append('Authorization',
+        `Bearer ${token.access}`);
+
+      const requestOptions = {
         method: 'GET',
-        headers: {
-          'Authorization': `Bearer ${token.access}`,
-        },
-      }).then(res => {
-        res.text()
-      });
+        headers: myHeaders,
+        redirect: 'follow',
+      };
+
+      fetch(SESAR_API + GET_USER_CODE, requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
+      // fetch(url, {
+      //   method: 'GET',
+      //   headers: {
+      //     'Authorization': `Bearer ${token.access}`,
+      //   },
+      // }).then((res) => {
+      //   res.text();
+      // });
       // const xmlData = await userCodeXml.text();
       // const jsonData = parse(xmlData);
       // const convert = require('xml-js');
@@ -65,9 +162,13 @@ const useSamples = () => {
 
   return {
     getAllSamplesCount: getAllSamplesCount,
-    getSesarUserCode: getSesarUserCode,
+    getOrcidToken: getOrcidToken,
+    getSesarToken: getSesarToken,
+    // getSesarUserCode: getSesarUserCode,
+    isOrcidSignInPrompt,
     onSampleFormChange: onSampleFormChange,
     selectedSampleData,
+
   };
 };
 
