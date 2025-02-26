64a0abfd4cdd4a53a7cf1c189fb8616bc29de618
jessicagood <jessicagood012@gmail.com>
2025-02-26 10:00:15 -0700

    feat(Report): Modified report label in Reports List

--- src/modules/reports/ReportsListItem.js
+++ src/modules/reports/ReportsListItem.js
@@ -5,6 +5,9 @@ import {useSelector} from 'react-redux';
 
 import commonStyles from '../../shared/common.styles';
 import {isEmpty} from '../../shared/Helpers';
+import {PRIMARY_TEXT_COLOR} from '../../shared/styles.constants';
+import {useForm} from '../form';
+import {PAGE_KEYS} from '../page/page.constants';
 import {useTags} from '../tags';
 
 const ReportsListItem = ({
@@ -16,9 +19,15 @@ const ReportsListItem = ({
   console.log('Rendering ReportsListItem', report.id, '...');
   console.log(report);
 
+  const {getLabel} = useForm();
   const {addRemoveSpotFromTag, getTagsAtSpot} = useTags();
 
   const selectedTag = useSelector(state => state.project.selectedTag);
+
+  const groupKey = 'general';
+  const pageKey = PAGE_KEYS.REPORTS;
+  const formName = [groupKey, pageKey];
+  const reportTypeLabel = report.report_type ? getLabel(report.report_type, formName) : 'No Type';
 
   const renderCheckboxes = () => {
     return (
@@ -42,7 +51,10 @@ const ReportsListItem = ({
       onPress={() => onPress(report)}
     >
       <ListItem.Content>
-        <ListItem.Title style={commonStyles.listItemTitle}>{report?.subject || 'No Subject'}</ListItem.Title>
+        <ListItem.Title style={[commonStyles.listItemTitle, {fontWeight: 'bold'}]}>{reportTypeLabel}</ListItem.Title>
+        <ListItem.Subtitle style={[commonStyles.listItemSubtitle, {color: PRIMARY_TEXT_COLOR}]}>
+          {report?.subject || 'No Subject'}
+        </ListItem.Subtitle>
         {doShowTags && report && renderTags()}
       </ListItem.Content>
       {isCheckedList ? renderCheckboxes() : report && <ListItem.Chevron/>}
