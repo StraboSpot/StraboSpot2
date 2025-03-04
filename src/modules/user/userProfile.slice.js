ef46ae31bf38ee093aa5774e30fc2e3f96349396
Nathan Novak <nnovak@ku.edu>
2025-03-04 13:44:52 -0700

    feat(IGSN): Stopped saving the Orcid token in Redux. Now it is passed directly to get a SESAR token and then cleared.  App will rely on SESAR access and refresh tokens to upload samples.

--- src/modules/user/userProfile.slice.js
+++ src/modules/user/userProfile.slice.js
@@ -8,17 +8,12 @@ const initialUserState = {
   isAuthenticated: false,
   mapboxToken: null,
   name: null,
-  orcidToken: {
-    token: null,
-    expiration: null,
-  },
   sesar: {
     selectedUserCode: '',
     userCodes: [],
     sesarToken: {
       access: null,
       refresh: null,
-      expiration: null,
     },
   },
 };
@@ -29,14 +24,10 @@ const userProfileSlice = createSlice({
   initialState: initialUserState,
   reducers: {
     setUserData(state, action) {
-      const {name, email, mapboxToken, orcidToken, encoded_login, image} = action.payload;
-      const tokenParsed = JSON.parse(atob(orcidToken.split('.')[1]));
-      const expDateTime = unixToDateTime(tokenParsed.exp).toUTCString();
+      const {name, email, mapboxToken, encoded_login, image} = action.payload;
       state.name = name;
       state.email = email;
       state.mapboxToken = mapboxToken;
-      state.orcidToken.token = orcidToken;
-      state.orcidToken.expiration = expDateTime;
       state.encoded_login = encoded_login;
       state.image = image;
     },
@@ -49,16 +40,10 @@ const userProfileSlice = createSlice({
     resetUserState() {
       return initialUserState;
     },
-    setOrcidToken(state, action) {
-      const {token, expiration} = action.payload;
-      state.orcidToken.token = token;
-      state.orcidToken.expiration = expiration;
-    },
     setSesarToken(state, action) {
-      const {access, refresh, expiration} = action.payload;
+      const {access, refresh} = action.payload;
       state.sesar.sesarToken.access = access;
       state.sesar.sesarToken.refresh = refresh;
-      state.sesar.sesarToken.expiration = expiration;
     },
     setSesarUserCodes(state, action) {
       state.sesar.userCodes = action.payload;
