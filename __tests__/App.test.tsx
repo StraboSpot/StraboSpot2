2b9e034fe2361c207048533301367e8c0245b542
jessicagood <jessicagood012@gmail.com>
2025-04-23 13:18:15 -0700

    build(Packages): Updated react-native to v0.79.1
    Includes changes and other package updates needed to get 0.79.1 working.

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,14 +2,12 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
-
-import {it} from '@jest/globals';   // Note: import explicitly to use the types shipped with jest.
-import renderer from 'react-test-renderer';   // Note: test renderer must be required after react-native.
-
+import ReactTestRenderer from 'react-test-renderer';
 import App from '../App';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('renders correctly', async () => {
+  await ReactTestRenderer.act(() => {
+    ReactTestRenderer.create(<App />);
+  });
 });
