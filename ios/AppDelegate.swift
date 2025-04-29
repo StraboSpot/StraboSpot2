aaa035ea85432f46214dcf4c908b09058c8b3474
Nathan Novak <nnovak@ku.edu>
2025-04-29 08:27:22 -0700

    build(IOS):Added Swift code in AppDelegate.swift for dep linking.

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -31,7 +31,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     return true
   }
+
+  func application(_ app: UIApplication,
+                   open url: URL,
+                   options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    return RCTLinkingManager.application(app, open: url, options: options)
+  }
 }
+
 
 class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
   override func sourceURL(for bridge: RCTBridge) -> URL? {
